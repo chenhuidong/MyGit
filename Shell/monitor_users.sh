769509822095b4c70bca13d6c52ae47e1893bfce
chenhuidong <chdyczx@live.com>
2015-04-17 20:49:36 +0800

    monitor_users.sh

--- Shell/monitor_users.sh
+++ Shell/monitor_users.sh
@@ -1,6 +1,12 @@
 #! /bin/bash
 
-v_users=`last | awk '{print $1}' | grep -v root | grep -v wtmp | grep -v chdyczx`
+v_users=`last | awk '{print $1 $3}' | grep -v root | grep -v wtmp | grep -v reboot | grep -v chdyczx`
+
+if [ ! -n "$v_users" ];
+then
+    echo "is null"
+    exit 0
+fi
 
 for v_user in $v_users
 do
