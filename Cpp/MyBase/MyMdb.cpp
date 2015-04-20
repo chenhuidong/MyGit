6a56e8858364506bd229347c71df1194862a4ffe
chenhuidong <chdyczx@live.com>
2015-04-20 14:52:17 +0800

    mdb over

--- Cpp/MyBase/MyMdb.cpp
+++ Cpp/MyBase/MyMdb.cpp
@@ -14,18 +14,5 @@ int  main(int argc, char const *argv[])
 	cout<<tabPocket[123]<<endl;
 	cout<<tabPocket[234]<<endl;
 
-
-
-	CPocket tabPocket1;
-	CPocketRecord rPocketRecord1;
-	rPocketRecord1.setPocketRecord(123, 234, 456);
-	tabPocket1.TInsert(rPocketRecord1.acct_id, rPocketRecord1);
-	
-	rPocketRecord1.setPocketRecord(234, 456, 567);
-    tabPocket1.TInsert(rPocketRecord1.acct_id, rPocketRecord1);	
-
-	//cout<<tabPocket[123].acct_id<<" "<<tabPocket[123].amount<<" "<<tabPocket[123].deduct_value<<endl;
-	cout<<tabPocket1[123]<<endl;
-	cout<<tabPocket1[234]<<endl;
 	return 0;
 }
