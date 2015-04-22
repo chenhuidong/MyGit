f4f12c5cd8d77aa8d05f847f9ca4a7a4406fba01
chenhuidong <chdyczx@live.com>
2015-04-22 17:28:51 +0800

    factory

--- Cpp/DesignPatterns/Factory/main.cpp
+++ Cpp/DesignPatterns/Factory/main.cpp
@@ -10,6 +10,6 @@ int main(int argc,char* argv[])
 	Product* p2 = fac->CreateProduct("ConcreteProduct2");
 	Product* p3 = fac->CreateProduct("ConcreteProduct3");
 
-	(ConcreteProduct1 *)p1->test();
+	((ConcreteProduct1 *)p1)->test();
 	return 0; 
 }
