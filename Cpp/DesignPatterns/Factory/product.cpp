b8cff8aa8886f67ddfa5311a49d4b8558dc5355c
chenhuidong <chdyczx@live.com>
2015-04-22 14:24:41 +0800

    factory

---
+++ Cpp/DesignPatterns/Factory/product.cpp
@@ -0,0 +1,15 @@
+//Product.cpp
+#include "Product.h"
+#include <iostream> 
+using namespace std;
+
+Product::Product() {}
+Product::~Product() {}
+
+ConcreteProduct::ConcreteProduct() 
+{ 
+	cout<<"ConcreteProduct...."<<endl; 
+}
+
+ConcreteProduct::~ConcreteProduct(){}
+
