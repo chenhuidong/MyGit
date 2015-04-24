5327f8c40b4e21c2633a5bed3801483502f241fa
chenhuidong <chdyczx@live.com>
2015-04-24 11:19:18 +0800

    bridge

--- Cpp/DesignPatterns/Bridge/main.cpp
+++ Cpp/DesignPatterns/Bridge/main.cpp
@@ -6,8 +6,13 @@ using namespace std;
 
 int main(int argc,char* argv[]) 
 { 
-	AbstractionImp* imp = new ConcreteAbstractionImpA();
-	Abstraction* abs = new RefinedAbstraction(imp);
-	abs->Operation(); 
+	AbstractionImp* imp1 = new ConcreteAbstractionImpA();
+	Abstraction* abs1 = new RefinedAbstraction(imp1);
+	abs1->Operation(); 
+
+	AbstractionImp* imp2 = new ConcreteAbstractionImpA();
+	Abstraction* abs2 = new RefinedAbstraction(imp2);
+	abs2->Operation(); 
+	
 	return 0; 
 }
