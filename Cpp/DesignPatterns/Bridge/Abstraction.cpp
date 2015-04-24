351aa45f82d6d8beca9968ceb9841be75a781d52
chenhuidong <chdyczx@live.com>
2015-04-24 11:15:56 +0800

    bridge

--- Cpp/DesignPatterns/Bridge/Abstraction.cpp
+++ Cpp/DesignPatterns/Bridge/Abstraction.cpp
@@ -1,12 +1,20 @@
 //Abstraction.cpp
-#include "Abstraction.h" #include "AbstractionImp.h"
-#include <iostream> using namespace std;
-Abstraction::Abstraction() {
+#include "Abstraction.h" 
+#include "AbstractionImp.h"
+#include <iostream> 
+using namespace std;
+
+Abstraction::Abstraction() {}
+Abstraction::~Abstraction() {}
+
+RefinedAbstraction::RefinedAbstraction(AbstractionImp* imp) 
+{ 
+	_imp = imp; 
 }
-Abstraction::~Abstraction() {
+
+RefinedAbstraction::~RefinedAbstraction() {}
+
+void RefinedAbstraction::Operation() 
+{ 
+	_imp->Operation(); 
 }
-RefinedAbstraction::RefinedAbstraction(AbstractionImp* imp) { _imp = imp; }
-RefinedAbstraction::~RefinedAbstraction()
-{
-}
-void RefinedAbstraction::Operation() { _imp->Operation(); }
