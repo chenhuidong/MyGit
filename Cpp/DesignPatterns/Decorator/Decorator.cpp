ab47209d3430ddf7963d51b22dbf6660554cdf4c
chenhuidong <chdyczx@live.com>
2015-04-28 10:30:55 +0800

    Decorator

--- Cpp/DesignPatterns/Decorator/Decorator.cpp
+++ Cpp/DesignPatterns/Decorator/Decorator.cpp
@@ -24,5 +24,12 @@ void Decorator::Operation() {}
 
 ConcreteDecorator::ConcreteDecorator(Component* com):Decorator(com) {}
 ConcreteDecorator::~ConcreteDecorator() {}
-void ConcreteDecorator::AddedBehavior() { std::cout<<"ConcreteDecorator::AddedBehacior...."<<std::endl; } void ConcreteDecorator::Operation() { _com->Operation();
-	this->AddedBehavior(); }
+void ConcreteDecorator::AddedBehavior() 
+{ 
+	std::cout<<"ConcreteDecorator::AddedBehacior...."<<std::endl; 
+} 
+void ConcreteDecorator::Operation() 
+{ 
+	_com->Operation();
+	this->AddedBehavior(); 
+}
