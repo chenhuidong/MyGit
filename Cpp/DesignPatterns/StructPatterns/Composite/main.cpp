699b64565b4f95726087ac8bf473ae2164843d3a
chenhuidong <chdyczx@live.com>
2015-04-29 17:25:18 +0800

    Composite

--- Cpp/DesignPatterns/StructPatterns/Composite/main.cpp
+++ Cpp/DesignPatterns/StructPatterns/Composite/main.cpp
@@ -9,10 +9,20 @@ int main(int argc,char* argv[])
 { 
 	Leaf* l = new Leaf(); 
 	l->Operation();
+	cout<<"1"<<endl<<endl;
+
 	Composite* com = new Composite();
 	com->Add(l);
 	com->Operation();
+	cout<<"2"<<endl<<endl;
+
 	Component* ll = com->GetChild(0);
 	ll->Operation();
+	cout<<"3"<<endl<<endl;
+
+	com->Remove(ll);
+	com->Operation();
+	cout<<"4"<<endl<<endl;
+
 	return 0; 
 }
