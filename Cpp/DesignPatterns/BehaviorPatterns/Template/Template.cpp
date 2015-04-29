aceab5b2e297ef12eab20c38a79cb3650794321e
chenhuidong <chdyczx@live.com>
2015-04-29 10:59:14 +0800

    Behavior

--- Cpp/DesignPatterns/BehaviorPatterns/Template/Template.cpp
+++ Cpp/DesignPatterns/BehaviorPatterns/Template/Template.cpp
@@ -14,27 +14,23 @@ ConcreteClass1::ConcreteClass1() {}
 ConcreteClass1::~ConcreteClass1() {} 
 void ConcreteClass1::PrimitiveOperation1()
 {
-	cout<<"ConcreteClass1...PrimitiveOperat
-	ion1"<<endl;
+	cout<<"ConcreteClass1...PrimitiveOperation1"<<endl;
 } 
 
 void ConcreteClass1::PrimitiveOperation2()
 {
-	cout<<"ConcreteClass1...PrimitiveOperat
-	ion2"<<endl;
+	cout<<"ConcreteClass1...PrimitiveOperation2"<<endl;
 } 
 
 ConcreteClass2::ConcreteClass2() {} 
 ConcreteClass2::~ConcreteClass2() {} 
 void ConcreteClass2::PrimitiveOperation1()
 {
-	cout<<"ConcreteClass2...PrimitiveOperat
-	ion1"<<endl;
+	cout<<"ConcreteClass2...PrimitiveOperation1"<<endl;
 } 
 
 void ConcreteClass2::PrimitiveOperation2()
 {
-	cout<<"ConcreteClass2...PrimitiveOperat
-	ion2"<<endl;
+	cout<<"ConcreteClass2...PrimitiveOperation2"<<endl;
 }
 
