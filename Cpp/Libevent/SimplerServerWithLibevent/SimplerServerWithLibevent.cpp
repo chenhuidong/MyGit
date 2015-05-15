833c909307b7fed1c21c7dfc8326ae3255bfca1c
chenhuidong <chdyczx@live.com>
2015-05-15 17:00:53 +0800

    SL

--- Cpp/Libevent/SimplerServerWithLibevent/SimplerServerWithLibevent.cpp
+++ Cpp/Libevent/SimplerServerWithLibevent/SimplerServerWithLibevent.cpp
@@ -85,7 +85,7 @@ errorcb(struct bufferevent *bev, short error, void *ctx)
 void
 do_accept(evutil_socket_t listener, short event, void *arg)
 {
-    struct event_base *base = arg;
+    struct event_base *base = (struct event_base *)arg;
     struct sockaddr_storage ss;
     socklen_t slen = sizeof(ss);
     int fd = accept(listener, (struct sockaddr*)&ss, &slen);
