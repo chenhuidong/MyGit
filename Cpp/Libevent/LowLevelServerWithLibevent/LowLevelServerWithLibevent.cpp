f7c8f3fa4e75e2cbe6b3ede1b509a5cd9f42edf3
chenhuidong <chdyczx@live.com>
2015-05-15 16:51:24 +0800

    L

--- Cpp/Libevent/LowLevelServerWithLibevent/LowLevelServerWithLibevent.cpp
+++ Cpp/Libevent/LowLevelServerWithLibevent/LowLevelServerWithLibevent.cpp
@@ -139,7 +139,7 @@ do_write(evutil_socket_t fd, short events, void *arg)
 void
 do_accept(evutil_socket_t listener, short event, void *arg)
 {
-    struct event_base *base = arg;
+    struct event_base *base = (struct event_base *)arg;
     struct sockaddr_storage ss;
     socklen_t slen = sizeof(ss);
     int fd = accept(listener, (struct sockaddr*)&ss, &slen);
