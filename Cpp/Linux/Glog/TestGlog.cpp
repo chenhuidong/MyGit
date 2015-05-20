090bd93519b368bb1c25ac7e281363f734269a11
chenhuidong <chdyczx@live.com>
2015-05-20 20:12:20 +0800

    Glog

--- Cpp/Linux/Glog/TestGlog.cpp
+++ Cpp/Linux/Glog/TestGlog.cpp
@@ -4,7 +4,8 @@ int main(int argc, char* argv[]) {
      // Initialize Google's logging library.
 	FLAGS_logtostderr = 0;
 	string t_strWorkPath = getenv("WORKPATH");
-	FLAGS_log_dir = t_strWorkPath + "$WORKPATH/Log";
+	cout<<t_strWorkPath<<endl;
+	FLAGS_log_dir = t_strWorkPath + "/Log";
 	google::InitGoogleLogging(argv[0]);
 
 	LOG_INFO << "Hello, Glog!";
