afeacf466eaf8ac8c50f6d37e1f4cb8acdcd8807
chenhuidong <chdyczx@live.com>
2015-05-20 15:09:02 +0800

    Glog

---
+++ Cpp/Linux/Glog/MyLog.h
@@ -0,0 +1,8 @@
+#ifndef __MY_LOG_H_
+#define __MY_LOG_H_
+
+#define LOG_INFO LOG(INFO)
+#define LOG_WARN LOG(WARNING)
+#define LOG_ERROR LOG(ERROR)
+#define LOG_FATAL LOG(FATAL)
+#endif
--- Cpp/Linux/Glog/TestGlog.cpp
+++ Cpp/Linux/Glog/TestGlog.cpp
@@ -1,11 +1,12 @@
 #include <glog/logging.h>
+#include "MyLog.h"
 
 int main(int argc, char* argv[]) {
      // Initialize Google's logging library.
-	//FLAGS_logtostderr = 0;
-	//FLAGS_log_dir = "/home/51linux_LxomB0aQ/MyProgram/MyGit/Cpp/Linux/Glog";
+	FLAGS_logtostderr = 0;
+	FLAGS_log_dir = "/home/51linux_LxomB0aQ/MyProgram/MyGit/Cpp/Linux/Glog";
 	google::InitGoogleLogging(argv[0]);
 
      // ...
-	LOG(INFO) << "Hello, Glog!";
+	LOG_INFO << "Hello, Glog!";
 }
