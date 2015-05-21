d5712f3ec4de42471de5bc990a6aa86fc553b288
chenhuidong <chdyczx@live.com>
2015-05-21 11:23:21 +0800

    Glog

--- Cpp/stdafx.h
+++ Cpp/stdafx.h
@@ -59,7 +59,6 @@ typedef vector<int> IntVec;
 #define INITIALIZE_LOG1(link)\
 {\
 	google::InitGoogleLogging(link);\
-	google::SetStderrLogging(google::GLOG_FATAL);\
 	FLAGS_logtostderr = 0;\
 	string t_strLogPath = getenv("LOGPATH");\
 	FLAGS_log_dir = t_strLogPath;\
@@ -69,19 +68,4 @@ typedef vector<int> IntVec;
 	google::SetLogDestination(google::FATAL, (string(FLAGS_log_dir)+"/"+link+".fatal.").c_str());\
 }
 
-/*	#ifdef DEBUG_MODE\
-        google::SetStderrLogging(google::GLOG_INFO); \
-    #else\
-        google::SetStderrLogging(google::GLOG_FATAL);\
-    #endif\
-    */
-//FLAGS_servitysinglelog = true;// 用来按照等级区分log文件
-//google::SetStderrLogging(google::GLOG_INFO);//设置级别高于 google::INFO 的日志同时输出到屏幕
-//FLAGS_colorlogtostderr = true; //设置输出到屏幕的日志显示相应颜色
-//FLAGS_logbufsecs = 0; //缓冲日志输出，默认为30秒，此处改为立即输出
-//FLAGS_max_log_size = 100; //最大日志大小为 100MB
-//FLAGS_stop_logging_if_full_disk = true; //当磁盘被写满时，停止日志输出
-//google::SetLogFilenameExtension("91_"); //设置文件名扩展，如平台？或其它需要区分的信息
-//google::InstallFailureSignalHandler(); //捕捉 core dumped (linux)
-//google::InstallFailureWriter(&Log); //默认捕捉 SIGSEGV 信号信息输出会输出到 stderr (linux)
 #endif
