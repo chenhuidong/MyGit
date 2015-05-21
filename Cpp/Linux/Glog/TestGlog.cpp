c6de670c0c8e242e3c1c4c29f3f62d3a54dc2cb6
chenhuidong <chdyczx@live.com>
2015-05-21 14:12:52 +0800

    Glog

--- Cpp/Linux/Glog/TestGlog.cpp
+++ Cpp/Linux/Glog/TestGlog.cpp
@@ -2,16 +2,9 @@
 
 int main(int argc, char* argv[]) {
      // Initialize Google's logging library.
-	/*
-	FLAGS_logtostderr = 0;
-	string t_strWorkPath = getenv("WORKPATH");
-	cout<<t_strWorkPath<<endl;
-	FLAGS_log_dir = t_strWorkPath + "/Log";
-	google::InitGoogleLogging(argv[0]);
-	*/
-
 	INITIALIZE_LOG();
+	
 	LOG_INFO << "Hello1, Glog!";
 	LOG_ERROR << "Hello2, Glog!";
-	LOG_FATAL << "Hello3, Glog!";
+	//LOG_FATAL << "Hello3, Glog!";
 }
--- Cpp/stdafx.h
+++ Cpp/stdafx.h
@@ -61,7 +61,7 @@ typedef vector<int> IntVec;
 	google::InitGoogleLogging(link);\
 	FLAGS_logtostderr = 0;\
 	FLAGS_alsologtostderr=0;\
-	FLAGS_stderrthreshold=4;\
+	FLAGS_stderrthreshold=google::FATAL;\
 	string t_strLogPath = getenv("LOGPATH");\
 	FLAGS_log_dir = t_strLogPath;\
 	google::SetLogDestination(google::INFO, (string(FLAGS_log_dir)+"/"+link+".info.").c_str());\
@@ -69,5 +69,4 @@ typedef vector<int> IntVec;
 	google::SetLogDestination(google::ERROR, (string(FLAGS_log_dir)+"/"+link+".error.").c_str());\
 	google::SetLogDestination(google::FATAL, (string(FLAGS_log_dir)+"/"+link+".fatal.").c_str());\
 }
-//google::FATAL
 #endif
