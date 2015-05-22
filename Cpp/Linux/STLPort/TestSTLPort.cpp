f6876d342ad55d4e3cd240d473ae307ca6bf6755
chenhuidong <chdyczx@live.com>
2015-05-22 17:03:00 +0800

    STLPort

---
+++ Cpp/Linux/STLPort/TestSTLPort.cpp
@@ -0,0 +1,23 @@
+// testSTLPort521.cpp : Defines the entry point for the console application.
+//
+#include "stdafx.h"
+#include <iostream>
+#include <hash_map>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    hash_map<int, string> mymap;
+    mymap[0x2011] = "liuxuezong";
+    mymap[0x7c00] = "STLport";
+    mymap[0x9000] = "hello hash_map!";
+    hash_map<int, string>::iterator iter = mymap.find(0x9000);
+    if (iter != mymap.end())
+    {
+        cout << iter->second << endl;
+    }
+    return 0;
+}
+
