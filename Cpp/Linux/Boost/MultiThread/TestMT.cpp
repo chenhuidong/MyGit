b8bf7d8276f14bf9fbf949aab42ca0733e412982
chenhuidong <chdyczx@live.com>
2015-05-26 18:06:02 +0800

    MT

--- Cpp/Linux/Boost/MultiThread/TestMT.cpp
+++ Cpp/Linux/Boost/MultiThread/TestMT.cpp
@@ -2,14 +2,14 @@
 #include <iostream>
 using namespace std;
 
-void my_func()
+/*void my_func()
 {
 	cout<<"thread test."<<endl;
-}
+}*/
 
 int main()
 {
-	boost::thread t(my_func);
-	t.join();
+	//boost::thread t(my_func);
+	//t.join();
 	return 0;
 }
