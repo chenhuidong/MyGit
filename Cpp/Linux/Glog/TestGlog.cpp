892c1d79a130e792b7cddb413558c40473f5beed
chenhuidong <chdyczx@live.com>
2015-05-29 16:44:40 +0800

    Glog

--- Cpp/Linux/Glog/TestGlog.cpp
+++ Cpp/Linux/Glog/TestGlog.cpp
@@ -1,8 +1,11 @@
 #include "../../stdafx.h"
 #include "../../MyLog.h"
 
-int main(int argc, char* argv[]) {
-     // Initialize Google's logging library.
+
+int main(int argc, char* argv[]) 
+{
+    // Initialize Google's logging library.
+    #define FLAGS_INFO
 	INITIALIZE_LOG(argv[0]);
 	
 	LOG_INFO << "Hello1, Glog!";
