936ac32eaf124eae776f2b3aa0a136df6a53ef93
chenhuidong <chdyczx@live.com>
2015-05-30 23:05:34 +0800

    MX

--- Cpp/Linux/TinyXml/TestMyXml.cpp
+++ Cpp/Linux/TinyXml/TestMyXml.cpp
@@ -10,6 +10,6 @@ int main()
     TiXmlElement *FirstPerson = oMyXml.m_RootElement->FirstChildElement();  
     cout << FirstPerson->Value() << endl;  
     TiXmlElement *NameElement = FirstPerson->FirstChildElement(); 
-    cout << NameElement->FirstChild()->Key()<< " " <<NameElement->FirstChild()->Value() << endl;   
+    cout << NameElement->Value()<< " " <<NameElement->FirstChild()->Value() << endl;   
 	return 0;
 }
