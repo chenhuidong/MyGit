877f170fd1c3ae36b51cfb1a8b1c8800c5725602
chenhuidong <chdyczx@live.com>
2015-06-01 11:18:10 +0800

    Perror

--- Cpp/Linux/Base/Process/Perror.cpp
+++ Cpp/Linux/Base/Process/Perror.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char *argv[])
 {
-	fprintf(stderr, "EACCES：%sn", strerror(EACCES));
+	fprintf(stderr, "EACCES：%s\n", strerror(EACCES));
 	errno = ENOENT;
 	perror(argv[0]);
 	return 0;
