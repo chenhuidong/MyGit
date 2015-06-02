8358cb031fed16dc54920f1490d4054b21f913c7
chenhuidong <chdyczx@live.com>
2015-06-02 16:19:32 +0800

    ACC

---
+++ Cpp/Linux/Base/Process/AvoidConditionCompetition.cpp
@@ -0,0 +1,35 @@
+#include "../../../stdafx.h"
+#include "../../../MyError.h"
+#include "../../../MySys.h"
+
+static void charatatime(const char *);
+
+int
+main(void)
+{
+	pid_t	pid;
+
+	TELL_WAIT();
+
+	if ((pid = fork()) < 0) {
+		err_sys("fork error");
+	} else if (pid == 0) {
+		WAIT_PARENT();		/* parent goes first */
+		charatatime("output from child\n");
+	} else {
+		charatatime("output from parent\n");
+		TELL_CHILD(pid);
+	}
+	exit(0);
+}
+
+static void
+charatatime(const char *str)
+{
+	const char	*ptr;
+	int		c;
+
+	setbuf(stdout, NULL);			/* set unbuffered */
+	for (ptr = str; (c = *ptr++) != 0; )
+		putc(c, stdout);
+}
