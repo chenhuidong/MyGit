aa149edd2cf2e93bd0522a267fede9521467db7e
chenhuidong <chdyczx@live.com>
2015-06-02 13:54:34 +0800

    Fork

---
+++ Cpp/Linux/Base/Process/ForkTwice.cpp
@@ -0,0 +1,27 @@
+#include "../../../stdafx.h"
+#include "../../../MyError.h"
+#include "../../../MySys.h"
+
+int main()
+{
+	pid_t pid;
+	if((pid=fork())<0)
+	{
+		err_sys("fork error");
+	}
+	else if(pid==0)
+	{
+		if((pid=fork())<0)
+			err_sys("fork error");
+		else if(pid>0)
+			exit(0);
+
+		sleep(2);
+		printf("second child, parent pid = %dn", getppid());
+		exit(0);
+	}
+
+	if(wait(pid, NULL, 0) != pid)
+		err_sys("waitpid error");
+	return 0;
+}
