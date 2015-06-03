ceba79b727eb321db7cb365eb67e66c832074ec9
chenhuidong <chdyczx@live.com>
2015-06-03 18:01:55 +0800

    CleanupThread

--- Cpp/Linux/Base/MultiThread/CleanupThread.cpp
+++ Cpp/Linux/Base/MultiThread/CleanupThread.cpp
@@ -12,8 +12,8 @@ void *
 thr_fn1(void *arg)
 {
 	printf("thread 1 start\n");
-	pthread_cleanup_push(cleanup, "thread 1 first handler");
-	pthread_cleanup_push(cleanup, "thread 1 second handler");
+	pthread_cleanup_push(cleanup, (void *)"thread 1 first handler");
+	pthread_cleanup_push(cleanup, (void *)"thread 1 second handler");
 	printf("thread 1 push complete\n");
 	if (arg)
 		return((void *)1);
@@ -26,8 +26,8 @@ void *
 thr_fn2(void *arg)
 {
 	printf("thread 2 start\n");
-	pthread_cleanup_push(cleanup, "thread 2 first handler");
-	pthread_cleanup_push(cleanup, "thread 2 second handler");
+	pthread_cleanup_push(cleanup, (void *)"thread 2 first handler");
+	pthread_cleanup_push(cleanup, (void *)"thread 2 second handler");
 	printf("thread 2 push complete\n");
 	if (arg)
 		pthread_exit((void *)2);
