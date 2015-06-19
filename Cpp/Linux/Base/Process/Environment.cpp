21f4f205b76d4d4c5d1f65e0f9204def3bb39399
chenhuidong <chdyczx@live.com>
2015-06-19 10:19:04 +0800

    En

--- Cpp/Linux/Base/Process/Environment.cpp
+++ Cpp/Linux/Base/Process/Environment.cpp
@@ -1,3 +1,4 @@
+/*
 #include <stdio.h>
 int main(void)
 { 
@@ -5,5 +6,16 @@ int main(void)
 	int i; 
 	for(i=0; environ[i]!=NULL; i++) 
 		printf("%s\n", environ[i]); 
+	return 0;
+}
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+int main(void)
+{ 
+	printf("PATH=%s\n", getenv("PATH")); 
+	//setenv("PATH", "hello", 1); 
+	//printf("PATH=%s\n", getenv("PATH")); 
 	return 0;
 }
