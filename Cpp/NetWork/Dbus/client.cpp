07bf29a4caea279eacc95a753fd1a34628c42ff5
chenhuidong <chdyczx@live.com>
2015-06-29 16:17:21 +0800

    dbus

--- Cpp/NetWork/Dbus/client.cpp
+++ Cpp/NetWork/Dbus/client.cpp
@@ -1,57 +1,53 @@
-/* server.c */
- 
 #include <dbus/dbus.h>
 #include <stdbool.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
  
-static DBusHandlerResult
-filter_func(DBusConnection *connection, DBusMessage *message, void *usr_data)
+int db_send(DBusConnection *dbconn)
 {
-    dbus_bool_t handled = false;
-    char *word = NULL;
-    DBusError dberr;
+    DBusMessage *dbmsg;
+    char *word;
+    int i;
  
-    if (dbus_message_is_signal(message, "client.signal.Type", "Test")) {
-        dbus_error_init(&dberr);
-        dbus_message_get_args(message, &dberr, DBUS_TYPE_STRING,
-            &word, DBUS_TYPE_INVALID);
-        if (dbus_error_is_set(&dberr)) {
-            dbus_error_free(&dberr);
-        } else {
-            printf("receive message %s\n", word);
-            handled = true;
-        }
+    dbmsg = dbus_message_new_signal("/client/signal/Object", // object name of the signal
+                                  "client.signal.Type",      // interface name of the signal
+                                  "Test");                   // name of the signal
+    if (!dbmsg) {
+        return -1;
     }
-    return (handled ? DBUS_HANDLER_RESULT_HANDLED : DBUS_HANDLER_RESULT_NOT_YET_HANDLED);
+ 
+    word = "hello world";
+    if (!dbus_message_append_args(dbmsg, DBUS_TYPE_STRING, &word, DBUS_TYPE_INVALID)) {
+        return -1;
+    }
+ 
+    if (!dbus_connection_send(dbconn, dbmsg, NULL)) {
+        return -1;
+    }
+    dbus_connection_flush(dbconn);
+    printf("send message %s\n", word);
+ 
+    dbus_message_unref(dbmsg);
+    return 0;
 }
  
 int main(int argc, char *argv[])
 {
     DBusError dberr;
     DBusConnection *dbconn;
+    int i;
  
     dbus_error_init(&dberr);
  
     dbconn = dbus_bus_get(DBUS_BUS_SESSION, &dberr);
     if (dbus_error_is_set(&dberr)) {
-        dbus_error_free(&dberr);
         return -1;
     }
  
-    if (!dbus_connection_add_filter(dbconn, filter_func, NULL, NULL)) {
-        return -1;
-    }
+    db_send(dbconn);
  
-    dbus_bus_add_match(dbconn, "type='signal',interface='client.signal.Type'", &dberr);
-    if (dbus_error_is_set(&dberr)) {
-        dbus_error_free(&dberr);
-        return -1;
-    }
+    dbus_connection_unref(dbconn);
  
-    while(dbus_connection_read_write_dispatch(dbconn, -1)) {
-        /* loop */
-    }
     return 0;
 }
