44bfd721edccdcc5db6d41b3c75caafc932f9f81
chenhuidong <chdyczx@live.com>
2015-07-07 11:04:50 +0800

    ipc

---
+++ Cpp/Linux/IPC/TestFtok.cpp
@@ -0,0 +1,18 @@
+#include "../../stdafx.h"
+#include "../../MyIPC.h"
+
+#define FLAGS_INFO
+#include "../../MyLog.h"
+
+int main(int argc, char **argv)
+{
+	INITIALIZE_LOG(argv[0]);
+
+	struct Stat stat;
+	if (argc != 2)
+		LOG_ERROR << "usage: ftok <pathname>";
+
+	Stat(argv[1], &Stat);
+	LOG_INFO << "st_dev:" <<stat.st_dev <<", st_info:" <<stat.st_info <<", key:" <<Ftok(argv[1], 0x57) <<endl;
+	return 0;
+}
