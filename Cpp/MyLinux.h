66494e491f457d40f7940ba8c436132e5be1a1a8
chenhuidong <chdyczx@live.com>
2015-07-07 18:05:38 +0800

    linux

---
+++ Cpp/MyLinux.h
@@ -0,0 +1,16 @@
+#ifndef __MY_LINUX_H_
+#define __MY_LINUX_H_
+
+#include "stdafx.h"
+#include "MySys.h"
+#include "MyFile.h"
+#include "MyIPC.h"
+#include "MySignal.h"
+
+
+#define FLAGS_INFO
+#include "MyLog.h"
+
+#include "MyXml.h"
+
+#endif
