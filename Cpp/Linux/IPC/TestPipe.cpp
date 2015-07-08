d782cb38b57efe63794264692148b3da34064c47
chenhuidong <chdyczx@live.com>
2015-07-08 13:49:06 +0800

    testpipe

--- Cpp/Linux/IPC/TestPipe.cpp
+++ Cpp/Linux/IPC/TestPipe.cpp
@@ -1,4 +1,8 @@
 #include "../../MyLinux.h"
+#include <errno.h>		/* for definition of errno */
+#include <stdarg.h>		/* ISO C variable aruments */
+
+void  client(int, int), server(int, int);
 
 int main()
 {
@@ -23,4 +27,46 @@ int main()
 
 	waitpid(childpid, NULL, 0);
 	return 0;
+}
+
+void client(int readfd, int writefd)
+{
+	size_t len;
+	size_t n;
+	char buf[BUFFSIZE];
+
+	fgets(buf, BUFFSIZE, stdin);
+	len=strlen(buf);
+
+	if(buf[len-1] == '\n')
+		len--;
+
+	write(writefd, buf, len);
+	while((n = read(readfd, buf, BUFFSIZE))>0)
+		write(STDOUT_FILENO, buf, n);
+}
+
+void server(int readfd, int writefd)
+{
+	int fd;
+	size_t n;
+	char buf[BUFFSIZE];
+
+	if((n=read(readfd, buf, BUFFSIZE)) == 0)
+		LOG_ERROR << "end-of-file while reading pathname" <<endl;
+
+	buf[n] = '\0';
+
+	if((fd = open(buf, O_RDONLY))<0)
+	{
+		snprintf(buf+n, sizeof(buf)-n, "can not open, %sn", stderror(errno));
+		n = strlen(buf);
+		write(writefd, buf, n);
+	}
+	else
+	{
+		while((n=read(fd, buf, BUFFSIZE))>0)
+			write(writefd, buf, n);
+		close(fd);
+	}
 }
