3a295b99387e91a43cd9f211e6a4da710964c8de
chenhuidong <chdyczx@live.com>
2015-07-10 11:39:33 +0800

    msgcreate

---
+++ Cpp/Linux/IPC/Testmsgcreate.cpp
@@ -0,0 +1,25 @@
+#include "../../MyLinux.h"
+
+int main(int argc, char** argv)
+{
+	INITIALIZE_LOG(argv[0]);
+
+	int c, oflag, mqid;
+
+	oflag = SVMSG_MODE | IPC_CREAT;
+	while((c = getopt(argc, argv,"e")) != -1)
+	{
+		swith(c)
+		{
+		case "e":
+			oflag != IPC_EXEL;
+			break;
+		}
+	}
+
+	if((optind != argc -1))
+		LOG_ERROR<< "usage: msgcreate [-e] <pathname>"<< endl;
+
+	mqid = msgget(ftok(argv[optind], 0), oflag);
+	return 0;
+}
