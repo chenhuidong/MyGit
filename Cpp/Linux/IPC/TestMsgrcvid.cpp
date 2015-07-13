a093802099a4a0661023103fe0c93db2124225e6
chenhuidong <chdyczx@live.com>
2015-07-13 22:01:35 +0800

    testmsgrcvid

---
+++ Cpp/Linux/IPC/TestMsgrcvid.cpp
@@ -0,0 +1,21 @@
+#include "../../MyLinux.h"
+#define MSGMAX (8192+sizeof(long))
+
+int main(int argc, char** argv)
+{
+	INITIALIZE_LOG(argv[0]);
+
+	int mqid;
+	ssize_t n;
+	struct msgbuf* buff;
+
+	if(argc != 2)
+		cout<< "usage: msgrcvid <mqid>"<< endl;
+
+	mqid = atoi(argv[1]);
+	buff = malloc(MSGMAX);
+
+	n = msgrcv(mqid, buff, MSGMAX, 0, 0);
+	printf("read %d bytes, type = %ldn", n, buff->mtype);
+	return 0;
+}
