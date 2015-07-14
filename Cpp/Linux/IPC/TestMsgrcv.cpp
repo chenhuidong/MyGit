919e5300f95c09423a99662187a0ceff283ff71b
chenhuidong <chdyczx@live.com>
2015-07-14 10:16:06 +0800

    msg

---
+++ Cpp/Linux/IPC/TestMsgrcv.cpp
@@ -0,0 +1,37 @@
+#include "../../MyLinux.h"
+#define MAXMSG (8192 + sizeof(long))
+
+int main(int argc, char** argv)
+{
+	INITIALIZE_LOG(argv[0]);
+
+	int c, flag, mqid;
+	long type;
+	ssize_t n;
+	struct msgbuf *buff;
+
+	type = flag = 0;
+
+	while((c = getopt(argc, argv,"nt:")) != -1)
+	{
+		switch(c)
+		{
+		case 'n':
+			flag |= IPC_NOWAIT;
+			break;
+		case 'e':
+			flag |= IPC_EXCL;
+			break;
+		}
+	}
+
+	if((optind != argc -1))
+		cout<< "usage: msgrcv [-n] [-t type] <pathname>"<< endl;
+
+	mqid = msgget(ftok(argv[optind], 0), MSG_R);
+	buff = (struct msgbuf *)malloc(MAXMSG);
+
+	n = msgrcv(mqid, buff, MAXMSG, type, flag);
+	printf("read %d bytes, type = %ld\n", n, buff->mtype);
+	return 0;
+}
