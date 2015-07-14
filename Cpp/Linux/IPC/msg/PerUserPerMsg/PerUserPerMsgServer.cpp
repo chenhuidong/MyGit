fd1b831a838cb408700f2b6d4e2336a36a8f039a
chenhuidong <chdyczx@live.com>
2015-07-14 16:48:11 +0800

    per

--- Cpp/Linux/IPC/msg/PerUserPerMsg/PerUserPerMsgServer.cpp
+++ Cpp/Linux/IPC/msg/PerUserPerMsg/PerUserPerMsgServer.cpp
@@ -2,6 +2,18 @@
 #include "../../../../MyLinux.h"
 
 void
+sig_chld(int signo)
+{
+	pid_t	pid;
+	int		stat;
+
+	while ( (pid = waitpid(-1, &stat, WNOHANG)) > 0)
+		;
+	return;
+}
+
+
+void
 server(int readid, int writeid)
 {
 	FILE	*fp;
