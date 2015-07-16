6419b1875333e7385fa24db4ce5beb917ea411b7
chenhuidong <chdyczx@live.com>
2015-07-16 16:09:25 +0800

    shm

---
+++ Cpp/Linux/IPC/shm/shmwrite.cpp
@@ -0,0 +1,23 @@
+#define __MY_ERROR_
+#include "../../../MyLinux.h"
+
+int
+main(int argc, char **argv)
+{
+	int		i, id;
+	struct shmid_ds	buff;
+	unsigned char	*ptr;
+
+	if (argc != 2)
+		err_quit("usage: shmwrite <pathname>");
+
+	id = shmget(ftok(argv[1], 0), 0, SVSHM_MODE);
+	ptr = shmat(id, NULL, 0);
+	shmctl(id, IPC_STAT, &buff);
+
+		/* 4set: ptr[0] = 0, ptr[1] = 1, etc. */
+	for (i = 0; i < buff.shm_segsz; i++)
+		*ptr++ = i % 256;
+
+	exit(0);
+}
