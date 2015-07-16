47c5c3e871097c033ce8f9abd824645c4b749dc1
chenhuidong <chdyczx@live.com>
2015-07-16 14:26:50 +0800

    shmrmid

---
+++ Cpp/Linux/IPC/shm/shmrmid.cpp
@@ -0,0 +1,16 @@
+#define __MY_ERROR_
+#include "../../../MyLinux.h"
+
+int
+main(int argc, char **argv)
+{
+	int		id;
+
+	if (argc != 2)
+		err_quit("usage: shmrmid <pathname>");
+
+	id = shmget(ftok(argv[1], 0), 0, SVSHM_MODE);
+	shmctl(id, IPC_RMID, NULL);
+
+	exit(0);
+}
