9f2a7cfd2c513bb97d20c187655f144af3eddabe
chenhuidong <chdyczx@live.com>
2015-07-16 14:10:56 +0800

    shm

--- Cpp/Linux/IPC/shm/shmget.cpp
+++ Cpp/Linux/IPC/shm/shmget.cpp
@@ -9,7 +9,7 @@ main(int argc, char **argv)
 	size_t	length;
 
 	oflag = SVSHM_MODE | IPC_CREAT;
-	while ( (c = Getopt(argc, argv, "e")) != -1) {
+	while ( (c = getopt(argc, argv, "e")) != -1) {
 		switch (c) {
 		case 'e':
 			oflag |= IPC_EXCL;
@@ -20,8 +20,8 @@ main(int argc, char **argv)
 		err_quit("usage: shmget [ -e ] <pathname> <length>");
 	length = atoi(argv[optind + 1]);
 
-	id = Shmget(Ftok(argv[optind], 0), length, oflag);
-	ptr = Shmat(id, NULL, 0);
+	id = shmget(ftok(argv[optind], 0), length, oflag);
+	ptr = shmat(id, NULL, 0);
 
 	exit(0);
 }
