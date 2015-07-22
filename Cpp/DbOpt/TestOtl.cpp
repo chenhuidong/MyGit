8d424d6ea71ed1950679ed4ec83e7f019304e342
chenhuidong <chdyczx@live.com>
2015-07-22 09:03:07 +0800

    testotl

--- Cpp/DbOpt/TestOtl.cpp
+++ Cpp/DbOpt/TestOtl.cpp
@@ -11,7 +11,7 @@ otl_connect db; // connect object
 void insert()
 // insert rows into table
 { 
- otl_stream o(50, // buffer size
+ otl_stream o(1, // buffer size
               "insert into test_tab values(:f1<float>,:f2<char[31]>)", 
                  // SQL statement
               db // connect object
