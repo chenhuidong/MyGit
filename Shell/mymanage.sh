3106a08855e62f46e635e77d5216e4e075c0d9e8
chenhuidong <chdyczx@live.com>
2015-07-28 11:30:21 +0800

    mymanage.sh

--- Shell/mymanage.sh
+++ Shell/mymanage.sh
@@ -20,7 +20,7 @@ hid=`id -u`
 huser=`whoami`
 
 #验证用户名密码是否正确
-vuser=`mr << ! | sed '1d'
+vuser=`mysql -h'121.42.27.147' -uroot -pAdmin_123 -P54321 -Dmysql << ! | sed '1d'
 select user from user where user = '$3' and password = PASSWORD('$4');
 !`
 
@@ -35,7 +35,7 @@ then
 fi
 
 #验证主机uid user 数据库用户 及数据库实例 
-vhid=`mr << ! | sed '1d'
+vhid=`mysql -h'121.42.27.147' -uroot -pAdmin_123 -P54321 -Dmysql << ! | sed '1d'
 select hid from userdatabaserel where hid = '$hid' and huser = '$huser' and duser = '$3' and dbname = '$2';
 !`
 
@@ -50,7 +50,7 @@ then
 fi
 
 #删除数据库用户及数据库实例
-mr << !
+mysql -h'121.42.27.147' -uroot -pAdmin_123 -P54321 -Dmysql << !
 drop user $3;
 drop database $2;
 delete from userdatabaserel where hid = '$hid' and huser = '$huser' and duser = '$3' and dbname = '$2';
@@ -69,7 +69,7 @@ hid=`id -u`
 huser=`whoami`
 
 #验证用户是否已创建过数据库实例
-vhid=`mr << ! | sed '1d'
+vhid=`mysql -h'121.42.27.147' -uroot -pAdmin_123 -P54321 -Dmysql << ! | sed '1d'
 select hid from userdatabaserel where hid = $hid;
 !`
 
@@ -84,7 +84,7 @@ then
 fi
 
 #创建数据库用户及实例并赋权
-mr << !
+mysql -h'121.42.27.147' -uroot -pAdmin_123 -P54321 -Dmysql << !
 create database $2;
 CREATE USER $3@'%' IDENTIFIED BY '$4';
 grant all on $2.*  to $3;
