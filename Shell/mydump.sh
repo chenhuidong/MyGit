86bdc07a90750c8876defc2b483011747c988317
chenhuidong <chdyczx@live.com>
2015-07-28 14:40:43 +0800

    dump

---
+++ Shell/mydump.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+shopt -s expand_aliases
+source $HOME/.bashrc
+date
+
+main()
+{
+	#备份
+	mysqldump -h'121.42.27.147' -uroot -pAdmin_123 -P54321  --all-databases > $WORKPATH2/bak/all_`date +%Y%m%d`.sql
+
+	#删除30天前文件
+	find $WORKPATH2/bak -type f -name "all*.sql" -ctime 30 -exec rm {} \; 
+}
+
+main $*
+
+exit 0
+
