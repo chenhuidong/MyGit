b5795190a1c1e29936fd02c193358572c7e0a477
chenhuidong <chdyczx@live.com>
2015-07-30 22:50:17 +0800

    testTimer

--- Cpp/Poco/Timer/testTimer.cpp
+++ Cpp/Poco/Timer/testTimer.cpp
@@ -18,7 +18,7 @@ int main(int argc, char** argv)
 	TimerExample te;
 	Timer timer(250, 500); // fire after 250ms, repeat every 500ms
 	timer.start(TimerCallback<TimerExample>(te, &TimerExample::onTimer));
-	Thread::sleep(5000);
+	Poco::Thread::sleep(5000);
 	timer.stop();
 	return 0;
 }
