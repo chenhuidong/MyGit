8acc745aabf2b3f2e4cf6b4420ed0bb351e36add
chenhuidong <chdyczx@live.com>
2015-07-30 16:07:14 +0800

    lib

--- Cpp/Poco/SharedLibrary/testSharedLibraryLoader.cpp
+++ Cpp/Poco/SharedLibrary/testSharedLibraryLoader.cpp
@@ -8,10 +8,11 @@ int main(int argc, char** argv)
 {
         std::string path("/mnt/home2/51linux_LxomB0aQ/MyProgram/MyGit/Cpp/Poco/SharedLibrary/libtestSharedLibrary");
         path.append(SharedLibrary::suffix()); // adds ".dll" or ".so"
-        std::cout<< path<<std::endl;
         SharedLibrary library(path); // will also load the library
         HelloFunc func = (HelloFunc) library.getSymbol("hello");
         func();
         library.unload();
         return 0;
 }
+
+//g++ testSharedLibraryLoader.cpp -omain -lPocoFoundation
