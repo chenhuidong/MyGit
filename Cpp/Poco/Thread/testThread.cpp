316823ed0cffaadbed724afb86621bd31dc8dece
chenhuidong <chdyczx@live.com>
2015-07-30 18:10:06 +0800

    testthread

--- Cpp/Poco/Thread/testThread.cpp
+++ Cpp/Poco/Thread/testThread.cpp
@@ -4,17 +4,17 @@
 
 class HelloRunnable: public Poco::Runnable
 {
-       virtual void run()
-       {
-            std::cout << "Hello, world!" << std::endl;
-       }
+	virtual void run()
+	{
+		std::cout << "Hello, world!" << std::endl;
+	}
 };
 
 int main(int argc, char** argv)
 {
-       HelloRunnable runnable;
-       Poco::Thread thread;
-       thread.start(runnable);
-       thread.join();
-       return 0;
+	HelloRunnable runnable;
+	Poco::Thread thread;
+	thread.start(runnable);
+	thread.join();
+	return 0;
 }
