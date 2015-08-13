bfb1ea316e6aa1cc1f57e8c9f5532f9d90c62d72
chenhuidong <chdyczx@live.com>
2015-08-13 11:29:29 +0800

    testpocolog

--- Cpp/Poco/Log/testPocoLog.cpp
+++ Cpp/Poco/Log/testPocoLog.cpp
@@ -9,6 +9,6 @@ int main(int argc, char* argv[])
     // Initialize Google's logging library.
 	INITIALIZE_LOG(argv[0]);
 	
-	LOG_INFO("This is a warning");
+	LOG_INFO("This is a warning %d", 1);
 	UNINITIALIZE_LOG();
 }
