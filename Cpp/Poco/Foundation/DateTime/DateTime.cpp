591ccd5f4163c1552423326a26d70034a2da3d5b
chenhuidong <chdyczx@live.com>
2015-08-28 13:56:14 +0800

    datatime

--- Cpp/Poco/Foundation/DateTime/DateTime.cpp
+++ Cpp/Poco/Foundation/DateTime/DateTime.cpp
@@ -37,7 +37,7 @@ int main(int argc, char** argv)
 	
 	DateTime dt;
 	int tzd;
-	DateTimeParser::parse(DateTimeFormat::ISO8601_FORMAT, str, dt, tzd);
+	DateTimeParser::parse(DateTimeFormat::HTTP_FORMAT, str, dt, tzd);
 
 	dt.makeUTC(tzd);
 	LocalDateTime ldt(tzd, dt);
