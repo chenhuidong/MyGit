bd71fbbe0511de5dfc846663a9077dfc6067f229
chenhuidong <chdyczx@live.com>
2015-09-01 11:45:54 +0800

    mail

--- Cpp/Poco/Net/Mail/Mail.cpp
+++ Cpp/Poco/Net/Mail/Mail.cpp
@@ -66,7 +66,8 @@ int main(int argc, char** argv)
 		message.addAttachment("logo", new StringPartSource(logo, "image/gif"));
 		
 		SMTPClientSession session(mailhost);
-		session.login();
+		//session.login();
+		session.loginUsingLogin("chdyczx@163.com","Chenhd@420462");
 		session.sendMessage(message);
 		session.close();
 	}
