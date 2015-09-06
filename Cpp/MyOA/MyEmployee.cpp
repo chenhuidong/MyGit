c9bc47f8536077ee9624be8e6767ee1dbaeced2b
chenhuidong <chdyczx@live.com>
2015-09-06 17:59:20 +0800

    mydata

--- Cpp/MyOA/MyEmployee.cpp
+++ Cpp/MyOA/MyEmployee.cpp
@@ -2,5 +2,19 @@
 
 int TEmployee::InsertData()
 {
+	    // insert some rows
+    Employee t_oEmployee = 
+    {
+        11,
+        "chenhuidong",
+        "chdyczx@live.com"
+    };
+
+    m_oStatement << "INSERT INTO Employee VALUES(?, ?, ?)",
+    use(t_oEmployee.empno),
+    use(t_oEmployee.name),
+    use(t_oEmployee.email);
+
+    m_oStatement.execute();
 	return 0;
 }
