d9874ef3ac4fd42f8480bace66361c1410203f1b
chenhuidong <chdyczx@live.com>
2015-09-07 11:06:55 +0800

    oa

--- Cpp/MyOA/MyEmployee.cpp
+++ Cpp/MyOA/MyEmployee.cpp
@@ -10,12 +10,12 @@ int OPTEmployee::InsertData()
 
 int OPTEmployee::SelectData()
 {
-	std::cout<< "1"<< std::endl;
+	//std::cout<< "1"<< std::endl;
 	//m_oSelectStatement << "SELECT Empno, Name, Email, ValidFlag FROM Employee",
 	//	into(m_oEmployees), now;
-	m_oSelectStatement << "SELECT Empno, Name, Email, ValidFlag FROM Employee",
-		into(m_oEmployees), now;
-	std::cout<< "2"<< std::endl;
+	//m_oSelectStatement << "SELECT Empno, Name, Email, ValidFlag FROM Employee",
+	//	into(m_oEmployees), now;
+	//std::cout<< "2"<< std::endl;
 	/*for (Employees::const_iterator it = m_oEmployees.begin(); it != m_oEmployees.end(); ++it)
 	{
 		std::cout << "Empno: " << it->get<0>() << 
@@ -23,5 +23,26 @@ int OPTEmployee::SelectData()
 			", Email: " << it->get<2>() << 
 			", ValidFlag: " << it->get<3>() <<std::endl;
 	}*/
+
+	struct Employee
+	{
+		int			empno;
+		std::string name;
+		std::string email;
+		int 		validflag;
+	} person;
+
+    m_oSelectStatement << "SELECT Empno, Name, Email, ValidFlag FROM Employee",
+        into(person.empno),
+        into(person.name),
+        into(person.email),
+        into(person.validflag),
+        range(0, 1); //  iterate over result set one row at a time
+
+    while (!select.done())
+    {
+        select.execute();
+        std::cout << person.empno << " " << person.name << " " << person.email << std::endl;
+    }
 	return 0;
 }
