3e99e751d76145115c15068afc4afcb06c896e36
chenhuidong <chdyczx@live.com>
2015-09-07 10:14:08 +0800

    oa

--- Cpp/MyOA/MyEmployee.cpp
+++ Cpp/MyOA/MyEmployee.cpp
@@ -4,7 +4,7 @@ int OPTEmployee::InsertData()
 {
 	std::cout<< "1"<< std::endl;
     m_oInsertStatement << "INSERT INTO Employee (Empno, Name, Email, ValidFlag) VALUES(:Empno, :Name, :Email, :ValidFlag)",
-    use(m_oEmployees), now;
+    	use(m_oEmployees), now;
 
     //m_oInsertStatement.execute();
 	return 0;
@@ -15,8 +15,7 @@ int OPTEmployee::SelectData()
 	Employees m_oEmployees;
 
 	m_oSelectStatement << "SELECT Empno, Name, Email, ValidFlag FROM Employee",
-	into(m_oEmployees),
-	now;
+		into(m_oEmployees),now;
 
 	for (Employees::const_iterator it = m_oEmployees.begin(); it != m_oEmployees.end(); ++it)
 	{
