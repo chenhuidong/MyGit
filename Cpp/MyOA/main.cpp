99f11519386b8920a980b97264212c6cd79516ba
chenhuidong <chdyczx@live.com>
2015-09-07 09:42:38 +0800

    oa

--- Cpp/MyOA/main.cpp
+++ Cpp/MyOA/main.cpp
@@ -9,18 +9,12 @@ int main()
 	//create table
     *t_oMyData.GetSession() << "CREATE TABLE IF NOT EXISTS Employee (Empno int primary key, Name VARCHAR(30), Email VARCHAR, ValidFlag interger(1) default 0)", now;
 
-    TEmployee t_oTEmployee(t_oMyData.GetSession());
+    OPTEmployee t_oOPTEmployee(t_oMyData.GetSession());
 
-    // insert some rows
-    Employee t_oEmployee = 
-    {
-    	15,
-    	"chenhuidong",
-    	"chdyczx@live.com"
-    };
 
-	//t_oTEmployee.InsertData(t_oEmployee);
-    t_oTEmployee.SelectData();
+    //t_oOPTEmployee.m_oEmployees.push_back(Employee(16,"chenhuidong","chdyczx@live.com"));
+	//t_oOPTEmployee.InsertData(t_oEmployee);
+    t_oOPTEmployee.SelectData();
 
     t_oMyData.Uninitialize();
 	return 0;
