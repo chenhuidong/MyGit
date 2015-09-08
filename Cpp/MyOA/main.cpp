2fd5d0a44199ca7e6c8b94734bed84c7fec9b692
chenhuidong <chdyczx@live.com>
2015-09-08 17:40:18 +0800

    oa

--- Cpp/MyOA/main.cpp
+++ Cpp/MyOA/main.cpp
@@ -3,6 +3,7 @@
 
 int main()
 {
+    /*
 	MyData t_oMyData;
 	t_oMyData.Initialize("employee.db");
 
@@ -37,6 +38,32 @@ int main()
     t_oEmployees.clear();
 
     t_oMyData.Uninitialize();
+    */
+
+    // parse an XML document and print the generated SAX events
+    if (argc < 2)
+    {
+        std::cout << "usage: " << argv[0] << ": <xmlfile>" << std::endl;
+        return 1;
+    }
+    
+    //MyHandler handler;
+
+    SAXParser parser;
+    parser.setFeature(XMLReader::FEATURE_NAMESPACES, true);
+    parser.setFeature(XMLReader::FEATURE_NAMESPACE_PREFIXES, true);
+    //parser.setContentHandler(&handler);
+    parser.setProperty(XMLReader::PROPERTY_LEXICAL_HANDLER, static_cast<LexicalHandler*>(&handler));
+    
+    try
+    {
+        parser.parse(argv[1]);
+    }
+    catch (Poco::Exception& e)
+    {
+        std::cerr << e.displayText() << std::endl;
+        return 2;
+    }
 	return 0;
 }
 
