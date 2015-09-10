2800217472c40bd9b57e722fecca077105dadfdf
chenhuidong <chdyczx@live.com>
2015-09-10 23:02:52 +0800

    oa

--- Cpp/MyOA/main.cpp
+++ Cpp/MyOA/main.cpp
@@ -30,24 +30,25 @@ int main()
     
     t_oOPTEmployee.SelectData(t_oEmployees);
     */
-    Employees t_oEmployees;
-    Employees t_oEmployees1;
-
-
-    t_oEmployees.push_back(Employee(20,"chenhuidong","chdyczx@live.com"));
-    t_oMyData.ExecuteSQL("INSERT INTO Employee (Empno, Name, Email) VALUES(:Empno, :Name, :Email)", t_oEmployees, t_oEmployees1);
-    t_oEmployees.clear();
+    Employees t_inEmployees;
+    Employees t_outEmployees;
 
     /*
-    t_oMyData.ExecuteSQL("SELECT Empno, Name, Email FROM Employee where ValidFlag = 0", t_oEmployees);
-    for (Employees::const_iterator it = t_oEmployees.begin(); it != t_oEmployees.end(); ++it)
+    t_oEmployees.push_back(Employee(20,"chenhuidong","chdyczx@live.com"));
+    t_oMyData.ExecuteSQL("INSERT INTO Employee (Empno, Name, Email) VALUES(:Empno, :Name, :Email)", t_inEmployees, t_outEmployees);
+    t_oEmployees.clear();
+    */
+
+    t_oMyData.ExecuteSQL("SELECT Empno, Name, Email FROM Employee where ValidFlag = 0", t_inEmployees, t_outEmployees);
+    for (Employees::const_iterator it = t_outEmployees.begin(); it != t_outEmployees.end(); ++it)
     {
         std::cout << "Empno: " << it->get<0>() << 
             ", Name: " << it->get<1>() << 
             ", Email: " << it->get<2>() << std::endl;
     }
-    t_oEmployees.clear();
-    */
+    t_inEmployees.clear();
+    t_outEmployees.clear();
+
     t_oMyData.Uninitialize();
 
 	return 0;
