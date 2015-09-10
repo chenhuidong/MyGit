32d707d97e838e7d81c1ecde9c1a605ffdbe6aa1
chenhuidong <chdyczx@live.com>
2015-09-10 22:42:12 +0800

    oa

--- Cpp/MyOA/MyData.h
+++ Cpp/MyOA/MyData.h
@@ -35,7 +35,7 @@ public:
 	int ExecuteSQL(const char* in_sSQL, T* in_oT)
 	{
 		Statement t_oStatement(*m_pSession);
-    	t_oStatement<< in_sSQL, use(*in_oT), now;
+    	t_oStatement<< in_sSQL, use(*in_oT), into(*in_oT), now;
     	return 0;
 	}
 private:
