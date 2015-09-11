baab0a901bff5e9c97babc2642e418551aa82ee3
chenhuidong <chdyczx@live.com>
2015-09-11 21:04:56 +0800

    oa

--- Cpp/MyOA/MyException.h
+++ Cpp/MyOA/MyException.h
@@ -1,6 +1,8 @@
 #ifndef __MY_OA_MY_EXCEPTION_H_
 #define __MY_OA_MY_EXCEPTION_H_
 
+#include "Poco/Exception.h"
+
 namespace Poco {
 
 POCO_DECLARE_EXCEPTION(Foundation_API, NoRecordException, Exception)
