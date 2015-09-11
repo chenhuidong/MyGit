b8281321e54edcdab9d05002b95e44c01ad54f5f
chenhuidong <chdyczx@live.com>
2015-09-11 15:38:41 +0800

    oa

--- Cpp/MyOA/main.cpp
+++ Cpp/MyOA/main.cpp
@@ -39,7 +39,7 @@ int main()
     t_oEmployees.clear();
     */
     char iSQL[1024] = {0};
-    snprintf(iSQL, sizeof(iSQL), "INSERT INTO Employee (Empno, Name, Email) VALUES(%d, %s, %s)", 2, "cccc", "fdsfs@163.com");
+    snprintf(iSQL, sizeof(iSQL), "INSERT INTO Employee (Empno, Name, Email) VALUES(%d, \'%s\', \'%s\')", 2, "cccc", "fdsfs@163.com");
     t_oMyData.ExecuteSQL(iSQL, t_outEmployees);
     /*
     t_oMyData.ExecuteSQL("SeleCT Empno, Name, Email FROM Employee where ValidFlag = 0", t_inoutEmployees);
