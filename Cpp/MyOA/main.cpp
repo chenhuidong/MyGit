7d1e1a1d7a48cd728e3a968a7a4288bec1b02793
chenhuidong <chdyczx@live.com>
2015-09-13 13:36:03 +0800

    oa

--- Cpp/MyOA/main.cpp
+++ Cpp/MyOA/main.cpp
@@ -121,10 +121,10 @@ int main()
         std::cout<< i<< std::endl;
         std::cout<< t_iIndex<< std::endl;
 
-        MyEmails::const_iterator it_MyEmails = t_outMyEmails[t_iIndex];
-        std::cout << "Sender: " << it_MyEmails->get<0>() << 
-            ", Password: " << it_MyEmails->get<1>() << 
-            ", Mailhost: " << it_MyEmails->get<2>() << std::endl;
+        //MyEmails::const_iterator it_MyEmails = t_outMyEmails[t_iIndex];
+        std::cout << "Sender: " << t_outMyEmails[t_iIndex]->get<0>() << 
+            ", Password: " << t_outMyEmails[t_iIndex]->get<1>() << 
+            ", Mailhost: " << t_outMyEmails[t_iIndex]->get<2>() << std::endl;
         
         i++;
     }
