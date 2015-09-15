1cb945f6bec62a55a3f1f7b72da104f3abd84727
chenhuidong <chdyczx@live.com>
2015-09-15 11:20:36 +0800

    oa

--- Cpp/MyOA/MyEmail.cpp
+++ Cpp/MyOA/MyEmail.cpp
@@ -3,6 +3,6 @@
 
 int main(int argc, char* argv[])
 {
-	execl("./sendemail", "smtp.163.com", "chdyczx@163.com", "Chenhd@420462", "chdyczx@live.com", NULL);
+	execl("./sendemail", "sendemail", "smtp.163.com", "chdyczx@163.com", "Chenhd@420462", "chdyczx@live.com", NULL);
 	return 0;
 }
