af7acd69fbd57ef20ff7b0f3bf1d6f5c0f10c0fc
chenhuidong <chdyczx@live.com>
2015-10-12 20:43:28 +0800

    oa

--- Cpp/MyOA/main.cpp
+++ Cpp/MyOA/main.cpp
@@ -5,6 +5,7 @@ int main(int argc, char* argv[])
 	MyAction t_oMyAction;
 	t_oMyAction.InitializeDb("employee.db");
 	t_oMyAction.Install();
+	t_oMyAction.InsertData();
 	t_oMyAction.SelectData();
 	t_oMyAction.UninitializeDb();
 	return 0;
