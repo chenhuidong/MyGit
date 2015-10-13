49b849d93956a2825ad8ff2a2b134553ab6b5d41
chenhuidong <chdyczx@live.com>
2015-10-13 20:06:38 +0800

    oa

--- Cpp/MyOA/MyAction.cpp
+++ Cpp/MyOA/MyAction.cpp
@@ -96,7 +96,7 @@ int MyAction::SelectEmployee(int in_iEmpno)
 int MyAction::CreateEmail()
 {
     std::wofstream ostr("/mnt/home2/51linux_LxomB0aQ/Log/1.html");
-    ostr << "<title>陈慧冬Huidong Chen&nbsp;201509工资单</title>\
+    ostr << L"<title>陈慧冬Huidong Chen&nbsp;201509工资单</title>\
 <body bgcolor=\"#fffcf2\" leftmargin=\"0\" topmargin=\"0\" marginwidth=\"0\"  marginheight=\"0\">\
 <h1 align=\"center\" style=\"font-family:微软雅黑;font-size=18pt;\"><br>工资通知单<br></h1>\
 <table width=100%><tr><td>\
