a5eda4f02ed723776664988ec3c8f0f10dc6c1b6
chenhuidong <chdyczx@live.com>
2015-10-19 11:51:18 +0800

    oa

--- Cpp/MyOA/MyEmail.cpp
+++ Cpp/MyOA/MyEmail.cpp
@@ -165,7 +165,7 @@ int MyEmail::CreateHtml(std::ostringstream &ostr)
   </td></tr></table>\r\n\
 </td></tr></table>" << std::endl;
 */
-  ostr << "test"<< std::endl;
+  ostr << "陈慧冬"<< std::endl;
 	return 0;
 }
 
