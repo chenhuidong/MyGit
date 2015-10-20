06634084d6d3cb4c0d8c8ddf52c9f7aad1b56eed
chenhuidong <chdyczx@live.com>
2015-10-20 19:38:57 +0800

    oa

--- Cpp/MyOA/MyStruct.h
+++ Cpp/MyOA/MyStruct.h
@@ -1,6 +1,9 @@
 #ifndef __MY_STRUCT_H_
 #define __MY_STRUCT_H_
 
+#include "Poco/Tuple.h"
+#include <vector>
+
 typedef Poco::Tuple<int, std::string, std::string> Employee;
 typedef std::vector<Employee> Employees;
 
