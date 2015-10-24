9781f6ba68b488b46fb5599a55f805f5a3e9264e
chenhuidong <chdyczx@live.com>
2015-10-24 10:40:17 +0800

    oa

--- Cpp/MyOA/MyEmail.cpp
+++ Cpp/MyOA/MyEmail.cpp
@@ -256,7 +256,7 @@ int MyEmail::SendEmails(MyDatas& in_oMyDatas)
   for(int i=0; i<2; i++)
   {
     Poco::Thread t_oThread;
-    t_aThread[i] = t_oThread;
+    t_aThread[i] = &t_oThread;
     t_oThread.start(MyEmail::SendEmail, (void *)(&in_oMyDatas));
   }
 
