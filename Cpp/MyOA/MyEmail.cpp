a6786b2e9d47f083e7f7470fa33ab2385331f23f
chenhuidong <chdyczx@live.com>
2015-10-28 17:07:27 +0800

    oa

--- Cpp/MyOA/MyEmail.cpp
+++ Cpp/MyOA/MyEmail.cpp
@@ -186,7 +186,7 @@ int MyEmail::CreateEmail(MailMessage &in_oMessage, Employee &in_oEmployee, Salar
     in_oSalary.get<4>()<< " "<< in_oSalary.get<5>()<< " "<< in_oSalary.get<6>()<< " "<< in_oSalary.get<7>()<< " "<<
     in_oSalary.get<8>()<< " "<< in_oSalary.get<9>()<< " "<< in_oSalary.get<10>()<< " "<< in_oSalary.get<11>();
   
-  int t_iEmpno = in_oEmployee.get<1>();
+  int t_iEmpno = in_oEmployee.get<0>();
   string t_sRecipient = in_oEmployee.get<2>();
 	//in_oMessage.setSender(sender);
 	in_oMessage.addRecipient(MailRecipient(MailRecipient::PRIMARY_RECIPIENT, t_sRecipient));
