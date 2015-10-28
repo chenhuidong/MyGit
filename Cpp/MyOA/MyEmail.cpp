fa84a5538615b24661d9bfd57a8265777b637b25
chenhuidong <chdyczx@live.com>
2015-10-28 10:07:06 +0800

    oa

--- Cpp/MyOA/MyEmail.cpp
+++ Cpp/MyOA/MyEmail.cpp
@@ -206,10 +206,10 @@ void MyEmail::SendEmail(void *in_pMyDatas)
   string t_sMailhost = t_pMyDatas->m_oEmails[t_iEmailIndex].get<2>();
   string t_sSender = t_pMyDatas->m_oEmails[t_iEmailIndex].get<0>();
   string t_sPassword = t_pMyDatas->m_oEmails[t_iEmailIndex].get<1>();
-  LOG_INFO<< "MailHost is"<< t_sMailhost;
+  LOG_INFO<< "MailHost is "<< t_sMailhost;
 
   SMTPClientSession t_oSession(t_sMailhost);
-
+  LOG_INFO<< "MailHost1 is "<< t_sMailhost;
   //t_oSession.login(SMTPClientSession::AUTH_LOGIN, t_sSender, t_sPassword);
   
   while(t_iEmployeeIndex < t_iEmployeesNum)
