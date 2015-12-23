0493fe870d019557920ba61154d93c28cc9a754a
chenhuidong <chdyczx@live.com>
2015-12-23 16:24:19 +0800

    lua

--- Lua/LoadLua.cpp
+++ Lua/LoadLua.cpp
@@ -1,4 +1,7 @@
 #include <stdio.h>  
+#include <unistd>
+#include <iostream>
+using namespace std;
 extern "C" {  
 #include "lua.h"  
 #include "lualib.h"  
@@ -8,29 +11,34 @@ extern "C" {
 lua_State* L;  
 int main ( int argc, char *argv[] ){  
     /*初始化Lua*/  
-L = luaL_newstate();  
+  L = luaL_newstate();  
   /*载入Lua基本库*/  
   luaL_openlibs(L);  
   /*加载lua脚本*/  
   luaL_dofile(L, "test.lua");  
   int iError = lua_pcall(L, 0, 0, 0);  
-    if (iError)  
-    {  
-        lua_close(L);  
-        return 1;  
-    }  
+  if (iError)  
+  {
+    cout<<"1" <<endl;  
+    lua_close(L);  
+    return 1;  
+  }  
+  cout<<"2" <<endl;  
     /*调用lua中的函数sum*/  
-    int a = 11 ;  
-    int b = 12 ;  
-    lua_getglobal(L,"add");               
-    lua_pushinteger(L,a) ;  
-    lua_pushinteger(L,b) ;  
-    int ret = lua_pcall(L,2,1,0) ;  
-    if ( ret != 0 )  
-      return 0;  
-    printf("sum:%d + %d = %ld\n",a,b,lua_tointeger(L,-1)) ;  
-    lua_pop(L,1);  
+  int a = 11 ;  
+  int b = 12 ;  
+  lua_getglobal(L,"add");               
+  lua_pushinteger(L,a) ;  
+  lua_pushinteger(L,b) ;  
+  int ret = lua_pcall(L,2,1,0) ;  
+  if ( ret != 0 )
+  {  
+    cout<<"3" <<endl;  
+    return 0; 
+  } 
+  printf("sum:%d + %d = %ld\n",a,b,lua_tointeger(L,-1)) ;  
+  lua_pop(L,1);  
     /* 清除Lua */  
   lua_close(L);  
   return 0;  
- } 
+} 
