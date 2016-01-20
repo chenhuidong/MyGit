130af779b32492775e240cb3f47a52ddce652cd2
chenhuidong <chdyczx@live.com>
2016-01-20 13:49:56 +0800

    echo

--- Cpp/poco-1.6.1-all/Net/samples/EchoServer/src/MyAnalyseRecv.h
+++ Cpp/poco-1.6.1-all/Net/samples/EchoServer/src/MyAnalyseRecv.h
@@ -3,15 +3,16 @@
 #include <iostream>
 #include <string>
 
-enum MyAnalyseType
-{
-	StringToMap,
-	XML,
-};
+
 
 class MyAnalyseRecv
 {
 public:
+	enum MyAnalyseType
+	{
+		StringToMap,
+		XML,
+	};
 	MyAnalyseRecv(MyAnalyseType t_eMyAnalyseType, std::string t_sRecv) : m_eMyAnalyseType(t_eMyAnalyseType), m_sRecv(t_sRecv){}
 	~MyAnalyseRecv(){}
 
