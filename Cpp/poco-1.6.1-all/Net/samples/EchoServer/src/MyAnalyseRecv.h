e0ce74e4d6dedec55f6dde54cd0480d23ec6eb03
chenhuidong <chdyczx@live.com>
2016-01-20 11:31:00 +0800

    analyse

--- Cpp/poco-1.6.1-all/Net/samples/EchoServer/src/MyAnalyseRecv.h
+++ Cpp/poco-1.6.1-all/Net/samples/EchoServer/src/MyAnalyseRecv.h
@@ -1,15 +1,17 @@
 #ifndef __MY_ANALYSE_RECV_H_
 #define __MY_ANALYSE_RECV_H_
+#include <iostream>
+#include <string>
 
 class MyAnalyseRecv
 {
 public:
-	MyAnalyseRecv():m_sRecv(t_sRecv){}
+	MyAnalyseRecv(std::string t_sRecv):m_sRecv(t_sRecv){}
 	~MyAnalyseRecv(){}
 
 	int AnalyseToMap();
 private:
-	string m_sRecv;
+	std::string m_sRecv;
 };
 
 #endif
