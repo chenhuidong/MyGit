7f44ac065c391caecdad146fca4f333113e0c52a
chenhuidong <chdyczx@live.com>
2016-01-21 22:36:45 +0800

    dealfactory

--- Cpp/poco-1.6.1-all/Net/samples/EchoServer/src/MyDealFactory.h
+++ Cpp/poco-1.6.1-all/Net/samples/EchoServer/src/MyDealFactory.h
@@ -1,6 +1,9 @@
 #ifndef __MY_DEAL_FACTORY_H_
 #define __MY_DEAL_FACTORY_H_
+#include "Poco/DynamicFactory.h"
 #include "MyDeal.h"
+
+using Poco::DynamicFactory;
 
 typedef Poco::DynamicFactory<MyDealBase> MyDealFactory; 
 
