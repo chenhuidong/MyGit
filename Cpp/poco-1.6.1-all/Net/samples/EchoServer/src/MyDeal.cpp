7977987826e343d9a2ea26fa99f4c6408658134f
chenhuidong <chdyczx@live.com>
2016-01-22 11:42:12 +0800

    echo

--- Cpp/poco-1.6.1-all/Net/samples/EchoServer/src/MyDeal.cpp
+++ Cpp/poco-1.6.1-all/Net/samples/EchoServer/src/MyDeal.cpp
@@ -10,7 +10,11 @@ MyDeal1::MyDeal1()
 	std::cout<<"MyDeal1...."<<std::endl; 
 }
 
-MyDeal1::~MyDeal1() {}
+MyDeal1::~MyDeal1() 
+{
+	std::cout<<"MyDeal1....end"<<std::endl; 
+}
+
 void MyDeal1::test() 
 {
 	std::cout<<"test1"<<std::endl;
@@ -22,7 +26,11 @@ MyDeal2::MyDeal2()
 	std::cout<<"MyDeal2...."<<std::endl; 
 }
 
-MyDeal2::~MyDeal2() {}
+MyDeal2::~MyDeal2() 
+{
+	std::cout<<"MyDeal2....end"<<std::endl; 
+}
+
 void MyDeal2::test() 
 {
 	std::cout<<"test2"<<std::endl;
