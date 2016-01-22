c079645bf395f444d022a2987cdb464bba289bf4
chenhuidong <chdyczx@live.com>
2016-01-22 12:22:19 +0800

    echo

--- Cpp/poco-1.6.1-all/Net/samples/EchoServer/src/MyDealFactory.cpp
+++ Cpp/poco-1.6.1-all/Net/samples/EchoServer/src/MyDealFactory.cpp
@@ -13,10 +13,10 @@ int MyDealFactory::CreateInstance(const std::string& in_sDealName)
 	assert (m_oDFactory.isClass(in_sDealName));  
 	m_oDFactory.createInstance(in_sDealName);
 
-    //MyDeal1* t_pMyDeal1 = dynamic_cast<MyDeal1*>(m_oDFactory.createInstance("MyDeal1"));  
+    MyDeal1* t_pMyDeal1 = dynamic_cast<MyDeal1*>(m_oDFactory.createInstance("MyDeal1"));  
     //MyDeal2* t_pMyDeal2 = dynamic_cast<MyDeal2*>(m_oDFactory.createInstance("MyDeal2"));  
 
-    //t_pMyDeal1->test();
+    t_pMyDeal1->test();
     //t_pMyDeal2->test();
 	return 0;
 }
