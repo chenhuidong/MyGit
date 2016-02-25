9b1014a2384db6c19e7e75ebf71c5ff374ef4470
chenhuidong <chdyczx@live.com>
2016-02-25 09:55:58 +0800

    lexical

--- Cpp/Boost/test_lexical_cast.cpp
+++ Cpp/Boost/test_lexical_cast.cpp
@@ -1,6 +1,8 @@
 #include <boost/lexical_cast.hpp>
+#include <boost/array.hpp>
 #include <vector>
 #include <iostream>
+#include <stdio.h>
 
 void log_message(const std::string &s)
 {
@@ -10,6 +12,13 @@ void log_message(const std::string &s)
 void log_errno(int yoko)
 {
 	log_message("Error " + boost::lexical_cast<std::string>(yoko) + ": " + strerror(yoko));
+}
+
+void number_to_file(int number, FILE* file)
+{
+	typedef boost::array<char, 50> buf_t; // You can use std::array if your compiler supports it
+	buf_t buffer = boost::lexical_cast<buf_t>(number); // No dynamic memory allocation
+	fputs(buffer.begin(), file);
 }
 
 int main(int /*argc*/, char * argv[])
@@ -35,6 +44,6 @@ int main(int /*argc*/, char * argv[])
 	}
 
 	log_errno(2);
-// ...
+	number_to_file(20, "1.txt");
 }
 
