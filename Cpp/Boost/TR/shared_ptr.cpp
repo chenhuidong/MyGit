b1aec2736c6d99f17464d198b6286fc533073855
chenhuidong <chdyczx@live.com>
2016-03-21 13:08:14 +0800

    shared_ptr

--- Cpp/Boost/TR/shared_ptr.cpp
+++ Cpp/Boost/TR/shared_ptr.cpp
@@ -9,7 +9,10 @@ int main()
 	shared_ptr<int> spi(new int);
 	assert(spi);
 	*spi = 253;
+	cout<< *spi<< endl;
+	
 	shared_ptr<string> sps(new string("smart"));
 	assert(sps->size() == 5);
+	cout<< sps->size()<< endl;
 	return 0;
 }
