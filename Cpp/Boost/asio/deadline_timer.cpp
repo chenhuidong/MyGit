3f1d6321e665656a359c59b69d2facbdbe9cc152
chenhuidong <chdyczx@live.com>
2016-03-21 22:49:00 +0800

    asio

---
+++ Cpp/Boost/asio/deadline_timer.cpp
@@ -0,0 +1,17 @@
+#include <boost/asio.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <iostream>
+using namespace boost::asio;
+using namespace std;
+
+int main()
+{
+	io_service ios;
+
+	deadline_timer t(ios, posix_time::seconds(2));
+	cout<< t.expires_at()<< endl;
+
+	t.wait();
+	cout<< "hello asio"<< endl;
+	return 0;
+}
