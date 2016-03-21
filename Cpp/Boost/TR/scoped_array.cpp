e059f14dbccbeb93342564e93d6534984377d3fd
chenhuidong <chdyczx@live.com>
2016-03-21 11:16:12 +0800

    scoped_array

---
+++ Cpp/Boost/TR/scoped_array.cpp
@@ -0,0 +1,24 @@
+#include <boost/smart_ptr.hpp>
+#include <iostream>
+#include <string>
+using namespace boost;
+using namespace std;
+
+int main()
+{
+	scoped_ptr<string> sa1(new int[100]);
+	sa1[0] = 10;
+	cout<< sa1[0]<<endl;
+
+	int arr* = new int[100];
+	scoped_array<int> sa(arr);
+
+	fill_n(&sa[0], 100, 5);
+
+	sa[10] = sa[20] + sa[30];
+
+	cout<< sa[10]<< " "<< sa[20]<< " "<< sa[30]<<endl;
+	return 0;
+}
+
+//g++ scoped_ptr.cpp -omain -lboost_math_tr1
