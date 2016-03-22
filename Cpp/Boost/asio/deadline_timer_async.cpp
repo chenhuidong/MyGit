792bf18802bee645547100421fa2e10c32eb3783
chenhuidong <chdyczx@live.com>
2016-03-22 09:37:45 +0800

    async

--- Cpp/Boost/asio/deadline_timer_async.cpp
+++ Cpp/Boost/asio/deadline_timer_async.cpp
@@ -2,6 +2,7 @@
 #include <boost/date_time/posix_time/posix_time.hpp>
 #include <iostream>
 using namespace boost;
+using namespace boost::asio;
 using namespace std;
 
 void print(system::error_code&)
