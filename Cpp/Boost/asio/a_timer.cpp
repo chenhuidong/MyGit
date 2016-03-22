0f312d1acf8438ee0ee6ea637e99e386ff1e6aef
chenhuidong <chdyczx@live.com>
2016-03-22 10:49:05 +0800

    a_timer

--- Cpp/Boost/asio/a_timer.cpp
+++ Cpp/Boost/asio/a_timer.cpp
@@ -1,5 +1,6 @@
 #include <boost/asio.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/function.hpp>
 #include <iostream>
 using namespace boost;
 using namespace boost::asio;
