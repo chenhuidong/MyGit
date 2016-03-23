6bdea16273487353e2b9306b568531713c40c677
chenhuidong <chdyczx@live.com>
2016-03-23 15:06:35 +0800

    clt

--- Cpp/Boost/asio/socket/async_socket_expired_clt.cpp
+++ Cpp/Boost/asio/socket/async_socket_expired_clt.cpp
@@ -37,7 +37,7 @@ public:
 
 		deadline_timer t(ios, boost::posix_time::seconds(5));
 		//t.async_wait(boost::bind(&client::time_expired, boost::asio::placeholders::error, boost::ref(sock)));
-		t.async_wait(boost::bind(&client::time_expired, this, boost::asio::placeholders::error, &sock));
+		t.async_wait(boost::bind(&client::time_expired, boost::asio::placeholders::error, &sock));
 		//start();
 	}
 
