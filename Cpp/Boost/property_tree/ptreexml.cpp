bedfacfe41458b0c19f72b31710f91b38c6f2ec1
chenhuidong <chdyczx@live.com>
2016-05-20 14:48:18 +0800

    ptree

--- Cpp/Boost/property_tree/ptreexml.cpp
+++ Cpp/Boost/property_tree/ptreexml.cpp
@@ -14,7 +14,7 @@ int main()
 	cout<< pt.get<long>("conf.gui")<< endl;
 	cout<< pt.get("conf.no_prop", 100)<< endl;
 
-	BOOST_AUTO(child, pt.get_child(conf.urls));
+	BOOST_AUTO(child, pt.get_child("conf.urls"));
 	for(BOOST_AUTO(pos, child.begin()); pos!= child.end(); ++pos)
 	{
 		cout<< pos->second.data()<< ",";
