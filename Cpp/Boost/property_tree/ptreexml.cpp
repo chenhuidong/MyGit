0e03201cabc2fd987b5157045505b3e410850baf
chenhuidong <chdyczx@live.com>
2016-05-20 14:46:25 +0800

    ptreexml

--- Cpp/Boost/property_tree/ptreexml.cpp
+++ Cpp/Boost/property_tree/ptreexml.cpp
@@ -1,5 +1,7 @@
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/xml_parser.hpp>
+#include <iostream>
+using namespace std;
 using namespace boost::property_tree;
 
 int main()
