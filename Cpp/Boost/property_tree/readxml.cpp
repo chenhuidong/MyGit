ab3b6151b76800e904138d0901e4a9e82040d848
chenhuidong <chdyczx@live.com>
2016-06-01 12:41:48 +0800

    readxml

--- Cpp/Boost/property_tree/readxml.cpp
+++ Cpp/Boost/property_tree/readxml.cpp
@@ -10,8 +10,8 @@ int main()
 	ptree pt;
 	read_xml("conf.xml", pt, xml_parser::no_concat_text | xml_parser::no_comments | xml_parser::trim_whitespace);
 
-	try
-	{
+	//try
+	//{
 		cout<< pt.get<string>("conf.theme")<< endl;
 		cout<< pt.get<int>("conf.clock_style")<< endl;
 		cout<< pt.get<long>("conf.gui")<< endl;
@@ -23,7 +23,7 @@ int main()
 			cout<< pos->second.data()<< ",";
 		}
 		cout<< endl;
-	}
+	/*}
 	catch(xml_parser::xml_parser_error &e)
 	{
 		cout<< e.what()<< endl;
@@ -32,6 +32,6 @@ int main()
 	{
 		cout<< e.what()<< endl;
 	}
-
+	*/
 	return 0;
 }
