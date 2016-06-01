291947a99025cfeade99d26058af775248831c23
chenhuidong <chdyczx@live.com>
2016-06-01 11:40:59 +0800

    readxml

--- Cpp/Boost/property_tree/readxml.cpp
+++ Cpp/Boost/property_tree/readxml.cpp
@@ -13,8 +13,8 @@ int main()
 	try
 	{
 		cout<< pt.get<string>("conf.theme")<< endl;
-		cout<< pt.get<int>("conf.clock_style")<< endl;
-		/*cout<< pt.get<long>("conf.gui")<< endl;
+		/*cout<< pt.get<int>("conf.clock_style")<< endl;
+		cout<< pt.get<long>("conf.gui")<< endl;
 		cout<< pt.get("conf.no_prop", 100)<< endl;	
 		
 		BOOST_AUTO(child, pt.get_child("conf.urls"));
