9dc501953061c9fcb80c9c0588f7d2cb0055ef60
chenhuidong <chdyczx@live.com>
2016-06-02 20:24:08 +0800

    MyDateTime

--- Cpp/Boost/MyBoost/MyDateTime/MyDateTime.cpp
+++ Cpp/Boost/MyBoost/MyDateTime/MyDateTime.cpp
@@ -1,29 +1,29 @@
 #include "MyDateTime.h"
 
-MMLIB::MyDateTime()
+MMLIB::MyDateTime::MyDateTime()
 {}
 
-MMLIB::~MyDateTime()
+MMLIB::MyDateTime::~MyDateTime()
 {}
 
-string& MMLIB::ToSimpleString(string& io_sDstDateTime, ptime& in_oDateTime)
+string& MMLIB::MyDateTime::ToSimpleString(string& io_sDstDateTime, ptime& in_oDateTime)
 {
 	return io_sDstDateTime = to_simple_string(in_oDateTime);
 }
 
-ptime& MMLIB::GetCurrentTime(ptime& io_oDateTime)
+ptime& MMLIB::MyDateTime::GetCurrentTime(ptime& io_oDateTime)
 {
 	return io_oDateTime = second_clock::local_time();
 }
 
-ptime& MMLIB::AddDays(ptime& io_oDstDateTime, ptime& in_oSrcDateTime, int in_iDays)
+ptime& MMLIB::MyDateTime::AddDays(ptime& io_oDstDateTime, ptime& in_oSrcDateTime, int in_iDays)
 {
 	date t_oSrcDateTime = in_oSrcDateTime.date();
 	date t_oDstDateTime = t_oSrcDateTime + days(in_iDays);
 	return io_oDstDateTime << t_oDstDateTime;
 }
 
-time_duration& MMLIB::GetDuration(ptime& in_oDateTimeLeft, ptime& in_oDateTimeRight, time_duration& ot_oDuration)
+time_duration& MMLIB::MyDateTime::GetDuration(ptime& in_oDateTimeLeft, ptime& in_oDateTimeRight, time_duration& ot_oDuration)
 {
 	return ot_oDuration = in_oDateTimeLeft - in_oDateTimeRight;
 }
