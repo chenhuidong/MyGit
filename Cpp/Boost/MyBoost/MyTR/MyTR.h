5532ad8864b6634451e1be2ee600beb6412af8d0
chenhuidong <chdyczx@live.com>
2016-06-02 11:15:44 +0800

    MyTR

--- Cpp/Boost/MyBoost/MyTR/MyTR.h
+++ Cpp/Boost/MyBoost/MyTR/MyTR.h
@@ -5,4 +5,10 @@
 #include <boost/smart_ptr.hpp>
 using namespace boost::smart_ptr;
 
+//auto_ptr<int> p;
+//scoped_ptr<int> sp(new int);
+//int *p = new int[100]; delete p[]; scoped_array<int> sa(arr); //vector代替
+//shared_ptr<int> spi(new int);
+//weak_ptr<int> wp(spi);
+
 #endif
