130a19bb154f63d889ccf18939ddd5cf7195c755
chenhuidong <chdyczx@live.com>
2016-06-07 18:04:23 +0800

    testMyDateTime

--- Cpp/Boost/MyBoost/MyDateTime/testMyDateTime.cpp
+++ Cpp/Boost/MyBoost/MyDateTime/testMyDateTime.cpp
@@ -7,5 +7,6 @@ int main()
 	//cout<< t_oMyDateTime.ToSimpleString(t_oMyDateTime.GetCurrentTime())<< endl;
 	//ptime t_oPtime = t_oMyDateTime.GetCurrentTime();
 	cout<< t_oMyDateTime.ToSimpleString(t_oMyDateTime.GetCurrentTime())<< endl;
+	cout<< t_oMyDateTime.ToSimpleString(t_oMyDateTime.AddDays(t_oMyDateTime.GetCurrentTime(), 1))<< endl;
 	return 0;
 }
