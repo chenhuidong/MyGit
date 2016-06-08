55d54ef1aed9a6226b400a6537364b533e532902
chenhuidong <chdyczx@live.com>
2016-06-08 15:03:02 +0800

    testMyDateTime

--- Cpp/Boost/MyBoost/MyDateTime/testMyDateTime.cpp
+++ Cpp/Boost/MyBoost/MyDateTime/testMyDateTime.cpp
@@ -6,7 +6,7 @@ int main()
 	IMyDateTime t_oMyDateTime;
 	//cout<< t_oMyDateTime.ToSimpleString(t_oMyDateTime.GetCurrentTime())<< endl;
 	//ptime t_oPtime = t_oMyDateTime.GetCurrentTime();
-	cout<< t_oMyDateTime.ToSimpleString(t_oMyDateTime.GetCurrentTime())<<"chd"<<second_clock::universal_time()<< endl;
+	cout<< /*t_oMyDateTime.ToSimpleString(t_oMyDateTime.GetCurrentTime())<<"chd"<<*/microsec_clock::universal_time()<< endl;
 	cout<< t_oMyDateTime.ToSimpleString(t_oMyDateTime.AddDays(t_oMyDateTime.GetCurrentTime(), 1))<< endl;
 	return 0;
 }
