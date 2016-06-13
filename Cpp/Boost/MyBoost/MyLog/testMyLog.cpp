98075acda3219fe42bc1b670a19e2827f04bd74a
chenhuidong <chdyczx@live.com>
2016-06-13 17:06:22 +0800

    testMyLog

--- Cpp/Boost/MyBoost/MyLog/testMyLog.cpp
+++ Cpp/Boost/MyBoost/MyLog/testMyLog.cpp
@@ -5,7 +5,7 @@
 int main(int argc, char* argv[]) 
 {
     // Initialize Google's logging library.
-	INITIALIZE_LOG(argv[0]);
+	MMyLib::INITIALIZE_LOG(argv[0]);
 	
 	LOG_INFO << "Hello1, Glog!";
 	LOG_ERROR << "Hello2, Glog!";
