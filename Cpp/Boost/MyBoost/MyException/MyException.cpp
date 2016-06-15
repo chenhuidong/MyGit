ebeb7a468d2578dc10a50eafbd4acf04e063b3e6
chenhuidong <chdyczx@live.com>
2016-06-15 15:55:43 +0800

    MyE

--- Cpp/Boost/MyBoost/MyException/MyException.cpp
+++ Cpp/Boost/MyBoost/MyException/MyException.cpp
@@ -1,4 +1,4 @@
 #include "MyException.h"
 
-POCO_IMPLEMENT_EXCEPTION(NoRecordException, Exception, "No record exception")
-POCO_IMPLEMENT_EXCEPTION(TestException, Exception, "Test exception")
+Poco::POCO_IMPLEMENT_EXCEPTION(NoRecordException, Exception, "No record exception")
+Poco::POCO_IMPLEMENT_EXCEPTION(TestException, Exception, "Test exception")
