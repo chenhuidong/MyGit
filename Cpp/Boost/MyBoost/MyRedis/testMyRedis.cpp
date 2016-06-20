edc73e25210026d138e405ded0390a5f2f5f4cbb
chenhuidong <chdyczx@live.com>
2016-06-20 15:38:46 +0800

    MyRedis

--- Cpp/Boost/MyBoost/MyRedis/testMyRedis.cpp
+++ Cpp/Boost/MyBoost/MyRedis/testMyRedis.cpp
@@ -8,7 +8,7 @@
 #include <adapters/libevent.h>
 
 void getCallback(redisAsyncContext *c, void *r, void *privdata) {
-    redisReply *reply = r;
+    redisReply *reply = (redisReply *)r;
     if (reply == NULL) return;
     printf("argv[%s]: %s\n", (char*)privdata, reply->str);
 
