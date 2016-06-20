5409f065828cc5b1609b6ebe05fda69a9f3ccfb5
chenhuidong <chdyczx@live.com>
2016-06-20 17:20:57 +0800

    MyRedis

--- Cpp/Boost/MyBoost/MyRedis/testMyRedis.cpp
+++ Cpp/Boost/MyBoost/MyRedis/testMyRedis.cpp
@@ -6,6 +6,6 @@ int main(int argc, char**argv)
     IMyRedis t_oMyRedis;
     t_oMyRedis.InitializeRedis();
     redisAsyncCommand(t_oMyRedis.m_pContext, NULL, NULL, "SET key %b", argv[argc-1], strlen(argv[argc-1]));
-    redisAsyncCommand(t_oMyRedis.m_pContext, GetCallback, (char*)"end-1", "GET key");
+    redisAsyncCommand(t_oMyRedis.m_pContext, IMyRedis::GetCallback, (char*)"end-1", "GET key");
     return 0;
 }
