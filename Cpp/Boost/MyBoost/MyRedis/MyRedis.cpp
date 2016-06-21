4f426a2d2c8693e39d73f440fd33eaa4d0889e32
chenhuidong <chdyczx@live.com>
2016-06-21 16:47:22 +0800

    MyRedis

--- Cpp/Boost/MyBoost/MyRedis/MyRedis.cpp
+++ Cpp/Boost/MyBoost/MyRedis/MyRedis.cpp
@@ -36,7 +36,7 @@ void MMyLib::IMyRedis::GetCallback(redisAsyncContext *c, void *r, void *privdata
     redisReply *reply = (redisReply *)r;
     if (reply == NULL) return;
     printf("argv[%s]: %s\n", (char*)privdata, reply->str);
-    LOG_INFO<< "argv["<< privdata<< "]"<< reply->str<< endl;
+    LOG_INFO<< "argv["<< (char*)privdata<< "]:"<< reply->str<< endl;
 }
 
 void MMyLib::IMyRedis::ConnectCallback(const redisAsyncContext *c, int status)
