467157e6ab4a689119a6bc6e2eccd69c4f636181
chenhuidong <chdyczx@live.com>
2016-06-28 17:05:07 +0800

    testMyDb

--- Cpp/Boost/MyBoost/MyDb/testMyDb.cpp
+++ Cpp/Boost/MyBoost/MyDb/testMyDb.cpp
@@ -4,7 +4,7 @@ int main(int argc, char** argv)
 {
 	MMyLib::INITIALIZE_LOG(argv[0]);
 	MMyLib::MyDb t_oMyDb;
-	t_oMyDb.Initialize(MMyLib::MyDb::SQLite, "testMyDb.db");
+	t_oMyDb.Initialize(3, "testMyDb.db");
 	t_oMyDb.Uninitialize();
 	return 0;
 }
