f4f54edbc49dabf5469b4c1e00ac364a48cd4475
chenhuidong <chdyczx@live.com>
2016-07-03 15:21:08 +0800

    test

--- Cpp/Boost/MyBoost/MyRedis/testMyRedis.cpp
+++ Cpp/Boost/MyBoost/MyRedis/testMyRedis.cpp
@@ -5,5 +5,9 @@ int main()
 	MMyLib::g_pConn->run(command("SET") << "foo" << "bar" );
 	reply t_oReply = MMyLib::g_pConn->run(command("GET") << "foo" );
 	std::cout << "FOO is: " << t_oReply.str() << std::endl;
+
+	MMyLib::g_pConn->run(command("hset") << "student.chenhuidong" << "name" );
+	reply t_oReply = MMyLib::g_pConn->run(command("GET") << "foo" );
+	std::cout << "FOO is: " << t_oReply.str() << std::endl;
 	return 0;
 }
