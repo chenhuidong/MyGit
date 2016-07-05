98d9a7867d90a882ebb52831acdcbe2c412482f1
chenhuidong <chdyczx@live.com>
2016-07-06 00:16:11 +0800

    MyClient

--- Cpp/Boost/MyBoost/MyNet/MyNet.cpp
+++ Cpp/Boost/MyBoost/MyNet/MyNet.cpp
@@ -1,44 +1,6 @@
 #include "MyNet.h"
 
-/*
-class MyServer
-{
-private:
-	io_service &ios;
-	ip::tcp::acceptor acceptor;
-	typedef std::shared_ptr<ip::tcp::socket> sock_pt;
-public:
-	MyServer(io_service& io): ios(io), acceptor(ios, ip::tcp::endpoint(ip::tcp::v4(), 54321))
-	{
-		start();
-	}
-
-	void start()
-	{
-		sock_pt sock(new ip::tcp::socket(ios));
-		acceptor.async_accept(*sock, boost::bind(&MyServer::accept_handler, this, boost::asio::placeholders::error, sock));		
-	}
-
-	void accept_handler(const boost::system::error_code& ec, sock_pt sock)
-	{
-		if(ec)
-			return ;
-		cout<< "clients:";
-		cout<< sock->remote_endpoint().address()<< endl;
-		sleep(20);
-		sock->async_write_some(buffer("hello asio"), \
-			boost::bind(&MyServer::write_handler, this, boost::asio::placeholders::error));
-
-		start();
-	}
-
-	void write_handler(const boost::system::error_code&)
-	{
-		cout<< "send msg complete."<< endl;
-	}
-};
-*/
-
+//MyServer
 MMyLib::MyServer::MyServer(io_service& io): ios(io), acceptor(ios, ip::tcp::endpoint(ip::tcp::v4(), MYPORT))
 {
 	start();
@@ -66,4 +28,35 @@ void MMyLib::MyServer::accept_handler(const boost::system::error_code& ec, sock_
 void MMyLib::MyServer::write_handler(const boost::system::error_code&)
 {
 	cout<< "send msg complete."<< endl;
+}
+
+//MyClient
+MMyLib::MyClient::MyClient(io_service& io): ios(io), ep(ip::address::from_string("127.0.0.1"), MYPORT)
+{
+	start();
+}
+
+void MMyLib::MyClient::start()
+{
+	sock_pt sock(new ip::tcp::socket(ios));
+	sock->async_connect(ep, boost::bind(&client::conn_handler, this, boost::asio::placeholders::error, sock));
+}
+
+void MMyLib::MyClient::conn_handler(const system::error_code& ec, sock_pt sock)
+{
+	if(ec)
+		return;
+
+	cout<< "recive from "<< sock->remote_endpoint().address()<< endl;
+	shared_ptr<vector<char> > str(new vector<char>(100, 0));
+
+	sock->async_read_some(buffer(*str), boost::bind(&client::read_handler, this, boost::asio::placeholders::error, str));
+}
+
+void MMyLib::MyClient::read_handler(const system::error_code& ec, shared_ptr<vector<char> > str)
+{
+	if(ec)
+		return;
+
+	cout<< &(*str)[0]<< endl<< endl;
 }
--- Cpp/Boost/MyBoost/MyNet/MyNet.h
+++ Cpp/Boost/MyBoost/MyNet/MyNet.h
@@ -9,44 +9,6 @@ using namespace boost::asio;
 
 namespace MMyLib
 {
-/*
-class MyServer
-{
-private:
-	io_service &ios;
-	ip::tcp::acceptor acceptor;
-	typedef std::shared_ptr<ip::tcp::socket> sock_pt;
-public:
-	MyServer(io_service& io): ios(io), acceptor(ios, ip::tcp::endpoint(ip::tcp::v4(), 54321))
-	{
-		start();
-	}
-
-	void start()
-	{
-		sock_pt sock(new ip::tcp::socket(ios));
-		acceptor.async_accept(*sock, boost::bind(&MyServer::accept_handler, this, boost::asio::placeholders::error, sock));		
-	}
-
-	void accept_handler(const boost::system::error_code& ec, sock_pt sock)
-	{
-		if(ec)
-			return ;
-		cout<< "clients:";
-		cout<< sock->remote_endpoint().address()<< endl;
-		sleep(20);
-		sock->async_write_some(buffer("hello asio"), \
-			boost::bind(&MyServer::write_handler, this, boost::asio::placeholders::error));
-
-		start();
-	}
-
-	void write_handler(const boost::system::error_code&)
-	{
-		cout<< "send msg complete."<< endl;
-	}
-};
-*/
 class MyServer
 {
 private:
@@ -59,6 +21,20 @@ public:
 	void accept_handler(const boost::system::error_code& ec, sock_pt sock);
 	void write_handler(const boost::system::error_code&);
 };
+
+class MyClient
+{
+private:
+	io_service& ios;
+	ip::tcp::endpoint ep;
+	typedef shared_ptr<ip::tcp::socket> sock_pt;
+public:
+	MyClient(io_service& io);
+	void start();
+	void conn_handler(const system::error_code& ec, sock_pt sock);
+	void read_handler(const system::error_code& ec, shared_ptr<vector<char> > str);
+};
+
 };
 
 #endif
