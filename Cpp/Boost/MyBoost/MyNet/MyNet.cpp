3cbc1902075cb555f783118a66f6dabb45c5d4cd
chenhuidong <chdyczx@live.com>
2016-07-12 11:25:46 +0800

    MyNet

--- Cpp/Boost/MyBoost/MyNet/MyNet.cpp
+++ Cpp/Boost/MyBoost/MyNet/MyNet.cpp
@@ -1,70 +1,25 @@
 #include "MyNet.h"
 
-//MyServer
-MMyLib::MyServer::MyServer(io_service& io): ios(io), acceptor(ios, ip::tcp::endpoint(ip::tcp::v4(), SERVPORT))
+MyServSession1::MyServSession1(boost::asio::io_service& ios): sock(new ip::tcp::socket(ios))
 {
-	start();
+
 }
 
-void MMyLib::MyServer::start()
+MyServSession1::~MyServSession1()
 {
-	//sock_pt sock(new ip::tcp::socket(ios));
-	session* new_session = new session(io_service_);
-	acceptor.async_accept(*sock, boost::bind(&MyServer::accept_handler, this, boost::asio::placeholders::error, sock));		
 }
 
-void MMyLib::MyServer::accept_handler(const boost::system::error_code& ec, sock_pt sock)
+void MyServSession1::start()
 {
-	/*
-	if(ec)
-		return ;
-	cout<< "MyClients:";
-	cout<< sock->remote_endpoint().address()<< endl;
-	sock->async_write_some(buffer("hello asio"), \
-		boost::bind(&MyServer::write_handler, this, boost::asio::placeholders::error));
-	*/
-    if (!ec)
-    {
-      new_session->start();
-    }
-    else
-    {
-      delete new_session;
-    }
-	start();
+	
 }
 
-void MMyLib::MyServer::write_handler(const boost::system::error_code&)
+void MyServSession1::write_handler(const boost::system::error_code&)
 {
-	cout<< "send msg complete."<< endl;
+
 }
 
-
-//MyClient
-MMyLib::MyClient::MyClient(io_service& io): ios(io), ep(ip::address::from_string(CLTIP), CLTPORT)
+void MyServSession1::read_handler(const system::error_code& ec, std::shared_ptr<vector<char> > str)
 {
-	start();
-}
 
-void MMyLib::MyClient::start()
-{
-	sock_pt sock(new ip::tcp::socket(ios));
-	sock->async_connect(ep, boost::bind(&MyClient::conn_handler, this, boost::asio::placeholders::error, sock));
-}
-
-void MMyLib::MyClient::conn_handler(const system::error_code& ec, sock_pt sock)
-{
-	if(ec)
-		return;
-	cout<< "recive from "<< sock->remote_endpoint().address()<< endl;
-	std::shared_ptr<vector<char> > str(new vector<char>(100, 0));
-
-	sock->async_read_some(buffer(*str), boost::bind(&MyClient::read_handler, this, boost::asio::placeholders::error, str));
-}
-
-void MMyLib::MyClient::read_handler(const system::error_code& ec, std::shared_ptr<vector<char> > str)
-{
-	if(ec)
-		return;
-	cout<< &(*str)[0]<< endl<< endl;
 }
--- Cpp/Boost/MyBoost/MyNet/MyNet.h
+++ Cpp/Boost/MyBoost/MyNet/MyNet.h
@@ -9,90 +9,38 @@ using namespace boost::asio;
 
 namespace MMyLib
 {
-class session
+typedef std::shared_ptr<ip::tcp::socket> sock_pt;
+
+class MySessionBase
 {
 public:
-  session(boost::asio::io_service& io_service)
-    : socket_(io_service)
-  {
-  }
-
-  tcp::socket& socket()
-  {
-    return socket_;
-  }
-
-  void start()
-  {
-    socket_.async_read_some(boost::asio::buffer(data_, max_length),
-        boost::bind(&session::handle_read, this,
-          boost::asio::placeholders::error,
-          boost::asio::placeholders::bytes_transferred));
-  }
-
-private:
-  void handle_read(const boost::system::error_code& error,
-      size_t bytes_transferred)
-  {
-    if (!error)
-    {
-      boost::asio::async_write(socket_,
-          boost::asio::buffer(data_, bytes_transferred),
-          boost::bind(&session::handle_write, this,
-            boost::asio::placeholders::error));
-    }
-    else
-    {
-      delete this;
-    }
-  }
-
-  void handle_write(const boost::system::error_code& error)
-  {
-    if (!error)
-    {
-      socket_.async_read_some(boost::asio::buffer(data_, max_length),
-          boost::bind(&session::handle_read, this,
-            boost::asio::placeholders::error,
-            boost::asio::placeholders::bytes_transferred));
-    }
-    else
-    {
-      delete this;
-    }
-  }
-
-  tcp::socket socket_;
-  enum { max_length = 1024 };
-  char data_[max_length];
+	MySessionBase(){};
+	virtual ~MySessionBase(){};
+public:
+    sock_pt sock;
 };
 
-class MyServer
+class MyServSessionBase: public MySessionBase
 {
-private:
-	io_service &ios;
-	ip::tcp::acceptor acceptor;
-	typedef std::shared_ptr<ip::tcp::socket> sock_pt;
 public:
-	MyServer(io_service& io);
+	MyServSessionBase(){};
+	virtual ~MyServSessionBase(){};
+
+	virtual void start() = 0;
+	virtual void write_handler(const boost::system::error_code&) = 0;
+	virtual void read_handler(const system::error_code& ec, std::shared_ptr<vector<char> > str) = 0;
+};
+
+class MyServSession1: public MyServSessionBase
+{
+public:
+	MyServSession1(boost::asio::io_service& io_service);
+	virtual ~MyServSession1();
+
 	void start();
-	void accept_handler(const boost::system::error_code& ec, sock_pt sock);
 	void write_handler(const boost::system::error_code&);
-};
-
-class MyClient
-{
-private:
-	io_service& ios;
-	ip::tcp::endpoint ep;
-	typedef std::shared_ptr<ip::tcp::socket> sock_pt;
-public:
-	MyClient(io_service& io);
-	void start();
-	void conn_handler(const system::error_code& ec, sock_pt sock);
 	void read_handler(const system::error_code& ec, std::shared_ptr<vector<char> > str);
 };
-
 };
 
 #endif
