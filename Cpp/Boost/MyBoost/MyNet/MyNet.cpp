cd786275694e550be5f1c0b4dd82d30b7c339d56
chenhuidong <chdyczx@live.com>
2016-07-12 11:30:26 +0800

    MyNet

--- Cpp/Boost/MyBoost/MyNet/MyNet.cpp
+++ Cpp/Boost/MyBoost/MyNet/MyNet.cpp
@@ -1,25 +1,25 @@
 #include "MyNet.h"
 
-MyServSession1::MyServSession1(boost::asio::io_service& ios): sock(new ip::tcp::socket(ios))
+MMyLib::MyServSession1::MyServSession1(boost::asio::io_service& ios): sock(new ip::tcp::socket(ios))
 {
 
 }
 
-MyServSession1::~MyServSession1()
+MMyLib::MyServSession1::~MyServSession1()
 {
 }
 
-void MyServSession1::start()
-{
-	
-}
-
-void MyServSession1::write_handler(const boost::system::error_code&)
+void MMyLib::MyServSession1::start()
 {
 
 }
 
-void MyServSession1::read_handler(const system::error_code& ec, std::shared_ptr<vector<char> > str)
+void MMyLib::MyServSession1::write_handler(const boost::system::error_code&)
+{
+
+}
+
+void MMyLib::MyServSession1::read_handler(const system::error_code& ec, std::shared_ptr<vector<char> > str)
 {
 
 }
