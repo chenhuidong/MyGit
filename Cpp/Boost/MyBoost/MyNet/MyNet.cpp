5d685cc9a30ec1dd0a502d6e5161dc2d586e652b
chenhuidong <chdyczx@live.com>
2016-07-12 14:18:45 +0800

    MyNet

--- Cpp/Boost/MyBoost/MyNet/MyNet.cpp
+++ Cpp/Boost/MyBoost/MyNet/MyNet.cpp
@@ -16,17 +16,27 @@ MMyLib::MyServSession1::MyServSession1(boost::asio::io_service& ios): MyServSess
 {}
 
 MMyLib::MyServSession1::~MyServSession1()
-{
-}
+{}
 
 void MMyLib::MyServSession1::start()
 {
+	//cout<< "recive from "<< sock->remote_endpoint().address()<< endl;
+	std::shared_ptr<vector<char> > str(new vector<char>(100, 0));
+	sock->async_read_some(buffer(*str), boost::bind(&MyServSession1::read_handler, this, boost::asio::placeholders::error, str));
 }
 
 void MMyLib::MyServSession1::write_handler(const boost::system::error_code&)
 {
+	if (ec)
+		return;
+	std::shared_ptr<vector<char> > str(new vector<char>(100, 0));
+	sock->async_read_some(buffer(*str), boost::bind(&MyServSession1::read_handler, this, boost::asio::placeholders::error, str));
 }
 
 void MMyLib::MyServSession1::read_handler(const system::error_code& ec, std::shared_ptr<vector<char> > str)
 {
+	if (ec)
+		return;
+	cout<< &(*str)[0]<< endl;
+	sock->async_write_some(buffer("hello asio"), boost::bind(&MyServSession1::write_handler, this, boost::asio::placeholders::error));
 }
--- Cpp/Boost/MyBoost/MyNet/MyNet.h
+++ Cpp/Boost/MyBoost/MyNet/MyNet.h
@@ -27,7 +27,7 @@ public:
 	virtual ~MyServSessionBase();
 
 	virtual void start() = 0;
-	virtual void write_handler(const boost::system::error_code&) = 0;
+	virtual void write_handler(const boost::system::error_code& ec) = 0;
 	virtual void read_handler(const system::error_code& ec, std::shared_ptr<vector<char> > str) = 0;
 };
 
@@ -38,7 +38,7 @@ public:
 	virtual ~MyServSession1();
 
 	void start();
-	void write_handler(const boost::system::error_code&);
+	void write_handler(const boost::system::error_code& ec);
 	void read_handler(const system::error_code& ec, std::shared_ptr<vector<char> > str);
 };
 };
