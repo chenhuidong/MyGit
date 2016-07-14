78941829c521abdc202f9d898959d2eae701e6b3
chenhuidong <chdyczx@live.com>
2016-07-14 09:27:16 +0800

    MyNet

--- Cpp/Boost/MyBoost/MyNet/MyNet.cpp
+++ Cpp/Boost/MyBoost/MyNet/MyNet.cpp
@@ -55,7 +55,7 @@ void MMyLib::MyServer::start()
 {
 	std::shared_ptr<MMyLib::MyServSession1> new_session(new MMyLib::MyServSession1(m_oIos));
 	//MyServSession1 *new_session = new  MyServSession1(m_oIos);
-	m_oAcceptor->async_accept(new_session->m_oSocket, boost::bind(&MyServer::accept_handler, this, new_session, boost::asio::placeholders::error));
+	m_oAcceptor.async_accept(new_session->m_oSocket, boost::bind(&MyServer::accept_handler, this, new_session, boost::asio::placeholders::error));
 }
 
 void MMyLib::MyServer::accept_handler(std::shared_ptr<MyServSession1> new_session, const boost::system::error_code& ec)
