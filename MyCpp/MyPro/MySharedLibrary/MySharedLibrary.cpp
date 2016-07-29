0bec5b3e534fb1e2413f61f5c2e957579a0beb22
chenhuidong <chdyczx@live.com>
2016-07-29 14:29:29 +0800

    testMySharedLibrary

--- MyCpp/MyPro/MySharedLibrary/MySharedLibrary.cpp
+++ MyCpp/MyPro/MySharedLibrary/MySharedLibrary.cpp
@@ -11,7 +11,7 @@ MMyLib::IMySharedLibrary::~IMySharedLibrary()
 void MMyLib::IMySharedLibrary::ExecFunc(string in_sFuncName)
 {
 	Func t_pFunc = (Func) m_oLibrary.getSymbol(in_sFuncName);
-	//if(!t_pFunc)
-	//	throw LoadFuncException("Func name not exist.");
+	if(!t_pFunc)
+		throw LoadFuncException("Func name not exist.");
 	t_pFunc();
 }
--- MyCpp/MyPro/MySharedLibrary/testMySharedLibrary.cpp
+++ MyCpp/MyPro/MySharedLibrary/testMySharedLibrary.cpp
@@ -2,7 +2,7 @@
 
 int main(int argc, char** argv)
 {
-	MMyLib::IMySharedLibrary m_oProvider("/home/chenhuidong/MyProgram/MyGit/MyCpp/MyPro/MySharedLibrary/libMySharedLibraryProvider.so");
+	MMyLib::IMySharedLibrary m_oProvider("/home/chenhuidong/MyProgram/MyGit/MyCpp/MyPro/MySharedLibrary/libMySharedLibraryProvider");
     m_oProvider.ExecFunc("hello");
     return 0;
 }
