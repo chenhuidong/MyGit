8a31d7fffa20b1b91a654509ddaf835e2cdad2c3
chenhuidong <chdyczx@live.com>
2016-07-29 15:12:40 +0800

    testSharedLibraryProvider

--- MyCpp/MyPro/MySharedLibrary/testSharedLibraryProvider.cpp
+++ MyCpp/MyPro/MySharedLibrary/testSharedLibraryProvider.cpp
@@ -1,23 +1,3 @@
-// TestLibrary.cpp
-#include <iostream>
-#if defined(_WIN32)
-#define LIBRARY_API __declspec(dllexport)
-#else
-#define LIBRARY_API
-#endif
-extern "C" void LIBRARY_API hello();
-void hello()
-{
-       std::cout << "Hello, world!" << std::endl;
-}
+#include "testMysharedLibrary.h"
 
-class PluginA
-{
-public:
-	void HelloWorld()
-	{
-       std::cout << "hello, world!" << std::endl;
-	}
-};
-
-//g++ testSharedLibraryProvider.cpp -fPIC -shared -o libtestSharedLibrary.so
+POCO_EXPORT_CLASS(PluginA)
