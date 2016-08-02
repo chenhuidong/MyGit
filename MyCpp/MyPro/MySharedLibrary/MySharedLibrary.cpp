42c47912bc14e25b3c337120d4e63db121def8c2
chenhuidong <chdyczx@live.com>
2016-08-02 14:29:03 +0800

    testMySharedLibrary

--- MyCpp/MyPro/MySharedLibrary/MySharedLibrary.cpp
+++ MyCpp/MyPro/MySharedLibrary/MySharedLibrary.cpp
@@ -1,9 +1,9 @@
 #include "MySharedLibrary.h"
 
-MMyLib::IMySharedLibrary::IMySharedLibrary(string in_sPathName): m_oLibrary(in_sPathName.append(SharedLibrary::suffix())), m_sPathName(in_sPathName.append(SharedLibrary::suffix()))
+MMyLib::IMySharedLibrary::IMySharedLibrary(string in_sPathName): /*m_oLibrary(in_sPathName.append(SharedLibrary::suffix())),*/ m_sPathName(in_sPathName.append(SharedLibrary::suffix()))
 {
 	//m_oLoader.loadLibrary(in_sFileName.append(SharedLibrary::suffix()));
-	cout<< in_sPathName<< endl;
+	cout<< m_sPathName<< endl;
 }
 
 MMyLib::IMySharedLibrary::~IMySharedLibrary()
--- MyCpp/MyPro/MySharedLibrary/testMySharedLibrary.cpp
+++ MyCpp/MyPro/MySharedLibrary/testMySharedLibrary.cpp
@@ -3,7 +3,7 @@
 int main(int argc, char** argv)
 {
 	MMyLib::IMySharedLibrary m_oProvider("/home/chenhuidong/MyProgram/MyGit/MyCpp/MyPro/MySharedLibrary/libMySharedLibraryProvider");
-    m_oProvider.ExecFunc("hello");
+    //m_oProvider.ExecFunc("hello");
     //m_oProvider.ExecClassFunc("PluginA", "HelloWorld");
     return 0;
 }
