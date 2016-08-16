c704d250b400e06b2dee6d360c0573fe7135b524
chenhuidong <chdyczx@live.com>
2016-08-16 09:55:24 +0800

    MyPipe

--- MyCpp/MyPro/MyTrans/MyPipe.cpp
+++ MyCpp/MyPro/MyTrans/MyPipe.cpp
@@ -1,14 +1,14 @@
 #include "MyStdAfx.h"
 
 int
-main(void)
+main(int argc, char**argv)
 {
 	MMyLib::INITIALIZE_LOG(argv[0]);
-	
+
 	int		n;
 	int		fd[2];
 	pid_t	pid;
-	char	line[MAXLINE];
+	char	line[MYSIZE];
 
 	if (pipe(fd) < 0)
 		LOG_ERROR<< "pipe error"<< endl;
@@ -19,7 +19,7 @@ main(void)
 		write(fd[1], "hello world\n", 12);
 	} else {				/* child */
 		close(fd[1]);
-		n = read(fd[0], line, MAXLINE);
+		n = read(fd[0], line, MYSIZE);
 		//write(STDOUT_FILENO, line, n);
 		LOG_INFO<< line<< endl;
 	}
