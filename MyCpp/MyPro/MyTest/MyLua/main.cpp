7a2d945ee8035fb9c8247d9ca113ed3ee5503300
chenhuidong <chdyczx@live.com>
2016-08-18 15:51:19 +0800

    testLua.cpp

--- MyCpp/MyPro/MyTest/MyLua/main.cpp
+++ MyCpp/MyPro/MyTest/MyLua/main.cpp
@@ -1,8 +1,28 @@
 #include "MyStdAfx.h"
+#ifdef __cplusplus                                                                                                      
+extern "C" {                                                                                   #endif                                                                           
+#include "tolua++.h"                                                               
+#ifdef __cplusplus
+}                                                                 
+#endif
+
+extern "C" {
+#include "lua.h"
+#include "lualib.h"
+#include "lauxlib.h"
+}
+
+#include "Test.h"
+
+int  tolua_Test_open (lua_State* tolua_S);
 
 int main(int argc, char**argv)
 {
 	MMyLib::INITIALIZE_LOG(argv[0]);
-
+    lua_State *L = luaL_newstate(); 
+    luaopen_base(L); 
+    tolua_Test_open(L);
+    luaL_dofile(L, "test.lua"); 
+    lua_close(L); 
 	return 0;
 }
--- MyCpp/MyPro/MyTest/MyLua/test.lua
+++ MyCpp/MyPro/MyTest/MyLua/test.lua
@@ -1,8 +1,6 @@
---lua函数定义，实现加法  
-function sum(a,b)  
-    return a+b ;  
-end
-
-function lua_add(a,b)
-   return add(a,b)
-end
+mytest = CTest:new()
+mytest:SetA(5)
+mytest:SetB("Hello")
+print(mytest:GetA())
+print(mytest:GetB())
+mytest:delete()
