603fbe5036e36f5f74e7c832544cdb025b9efe08
chenhuidong <chdyczx@live.com>
2016-08-19 10:32:22 +0800

    main.cpp

--- MyCpp/MyPro/MyTest/MyLua/main.cpp
+++ MyCpp/MyPro/MyTest/MyLua/main.cpp
@@ -18,28 +18,28 @@ int  tolua_Test_open (lua_State* tolua_S);
 int main(int argc, char**argv)
 {
 	MMyLib::INITIALIZE_LOG(argv[0]);
-    lua_State *L = luaL_newstate(); 
     
+    lua_State *L = luaL_newstate(); 
     luaopen_base(L); 
     
     /*载入Lua基本库*/  
-  	luaL_openlibs(L);  
+  	//luaL_openlibs(L);
     tolua_Test_open(L);
 
-    //luaL_dofile(L, "test.lua"); 
-    
+    luaL_dofile(L, "test1.lua");
+    //luaL_loadfile(L,"test1.lua");
+
     int a = 1, b=2;
-    luaL_loadfile(L,"test1.lua");
     lua_getglobal(L,"sum");
     lua_pushinteger(L,a);
     lua_pushinteger(L,b);
     int t_iReturn = lua_pcall(L,2,1,0);
-	cout<< t_iReturn<< endl;  
+	cout<< t_iReturn<< endl;
     if (t_iReturn)                       // 调用出错  
     {  
         const char *pErrorMsg = lua_tostring(L, -1);  
         cout<< pErrorMsg<< endl;  
-        lua_close(L);  
+        lua_close(L);
         return -1;  
     }
 
