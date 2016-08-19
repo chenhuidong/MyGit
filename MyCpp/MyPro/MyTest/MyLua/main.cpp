71f0475a46975467e8d1a8a9c3816f64dc3b2432
chenhuidong <chdyczx@live.com>
2016-08-19 10:36:00 +0800

    main.cpp

--- MyCpp/MyPro/MyTest/MyLua/main.cpp
+++ MyCpp/MyPro/MyTest/MyLua/main.cpp
@@ -24,18 +24,16 @@ int main(int argc, char**argv)
     
     /*载入Lua基本库*/  
   	//luaL_openlibs(L);
-    tolua_Test_open(L);
+  	luaL_dofile(L, "test1.lua");
 
-    luaL_dofile(L, "test1.lua");
-    //luaL_loadfile(L,"test1.lua");
+    tolua_Test_open(L);
 
     int a = 1, b=2;
     lua_getglobal(L,"sum");
     lua_pushinteger(L,a);
     lua_pushinteger(L,b);
     int t_iReturn = lua_pcall(L,2,1,0);
-	cout<< t_iReturn<< endl;
-    if (t_iReturn)                       // 调用出错  
+    if (t_iReturn)				// 调用出错  
     {  
         const char *pErrorMsg = lua_tostring(L, -1);  
         cout<< pErrorMsg<< endl;  
@@ -43,7 +41,7 @@ int main(int argc, char**argv)
         return -1;  
     }
 
-    if(lua_isnumber(L, -1))        //取值输出  
+    if(lua_isnumber(L, -1))		//取值输出  
     {  
         double fValue = lua_tonumber(L, -1);  
         cout << "Result is " << fValue << endl;  
