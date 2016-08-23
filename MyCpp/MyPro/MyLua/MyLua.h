59574dac92d484f958b20f7c59aec45b4ee96423
chenhuidong <chdyczx@live.com>
2016-08-23 14:16:05 +0800

    MyLua

--- MyCpp/MyPro/MyLua/MyLua.h
+++ MyCpp/MyPro/MyLua/MyLua.h
@@ -23,8 +23,8 @@ namespace MMyLib
 
 	#define LUAINIT(in_sFileName) {\
 		g_pLua = luaL_newstate();\
-		luaopen_base(L);\
-		luaL_dofile(L, in_sFileName);}
+		luaopen_base(g_pLua);\
+		luaL_dofile(g_pLua, in_sFileName);}
 };
 
 #endif
