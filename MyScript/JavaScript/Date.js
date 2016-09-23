25b4eabc9b049877b041c4b71389467da8e26382
chenhuidong <chdyczx@live.com>
2016-09-23 11:14:05 +0800

    js

---
+++ MyScript/JavaScript/Date.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head> 
+<meta charset="utf-8"> 
+<title>菜鸟教程(runoob.com)</title> 
+</head>
+<body>
+
+<button onclick="getElementById('demo').innerHTML=Date()">现在的时间是?</button>
+<p id="demo"></p>
+<button onclick="this.innerHTML=Date()">现在的时间是?</button>
+
+</body>
+</html>
