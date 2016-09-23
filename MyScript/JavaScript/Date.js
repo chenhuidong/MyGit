596cfe733331258a87d21c6ecaf1f25d84387099
chenhuidong <chdyczx@live.com>
2016-09-23 11:16:39 +0800

    js

--- MyScript/JavaScript/Date.js
+++ MyScript/JavaScript/Date.js
@@ -9,6 +9,14 @@
 <button onclick="getElementById('demo').innerHTML=Date()">现在的时间是?</button>
 <p id="demo"></p>
 <button onclick="this.innerHTML=Date()">现在的时间是?</button>
+<p>点击按钮执行 <em>displayDate()</em> 函数.</p>
+<button onclick="displayDate()">点这里</button>
+<script>
+function displayDate(){
+	document.getElementById("demo1").innerHTML=Date();
+}
+</script>
+<p id="demo1"></p>
 
 </body>
 </html>
