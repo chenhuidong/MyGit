1a9de5bb1c82ec2050bff4abdf6c973559f3c81b
chenhuidong <chdyczx@live.com>
2016-09-30 17:49:02 +0800

    testMyRedis

--- MyCpp/MyPro/MyRedis/testMyRedis.cpp
+++ MyCpp/MyPro/MyRedis/testMyRedis.cpp
@@ -4,7 +4,8 @@ int main()
 {
 	const char* t_sStr = "bar";
 	MMyLib::g_pRedisConn->run(command("SET") << "foo" << t_sStr );
-	reply t_oReply = MMyLib::g_pRedisConn->run(command("GET") << "foo" );
+	const char* t_sStr1 = "foo";
+	reply t_oReply = MMyLib::g_pRedisConn->run(command("GET") << t_sStr1 );
 	std::cout << "FOO is: " << t_oReply.str() << std::endl;
 
 	MMyLib::g_pRedisConn->run(command("hset")<< "student.chenhuidong"<< "name"<< "chenhuidong");
