f980cdbbf7e053998550e66bc1971e55c154a3b5
chenhuidong <chdyczx@live.com>
2016-09-30 15:24:54 +0800

    MyFrame

--- MyCpp/MyPro/MyWeb/MyFrame.cpp
+++ MyCpp/MyPro/MyWeb/MyFrame.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
+#include <stdlib.h>
 using namespace std;
 
-int main(int argc, int **argv)
+int main(int argc, int *argv[])
 {
 	if(argc != 2)
 		cout<< "need id"<< endl;
 
-	int t_iConditionId = argv[2];
+	int t_iConditionId = atoi(argv[2]);
 	//获取id
 	cout<< t_iConditionId<< " kkk"<< endl;
 
