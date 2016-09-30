e6bba51753dd8255fad97771550524a8a0e9de18
chenhuidong <chdyczx@live.com>
2016-09-30 15:22:37 +0800

    MyFrame

--- MyCpp/MyPro/MyWeb/MyFrame.cpp
+++ MyCpp/MyPro/MyWeb/MyFrame.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int main()
+int main(int argc, int **argv)
 {
 	if(argc != 2)
 		cout<< "need id"<< endl;
@@ -9,7 +9,7 @@ int main()
 	int t_iConditionId = argv[2];
 	//获取id
 	cout<< t_iConditionId<< " kkk"<< endl;
-	
+
 	//解析xml
 	//执行程序
 	return 0;
