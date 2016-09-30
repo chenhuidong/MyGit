ccb4afc10b40af2ec26dbd499b225715dc096994
chenhuidong <chdyczx@live.com>
2016-09-30 17:08:20 +0800

    MyFrame

--- MyCpp/MyPro/MyWeb/MyFrame.cpp
+++ MyCpp/MyPro/MyWeb/MyFrame.cpp
@@ -6,10 +6,11 @@ int main(int argc, char *argv[])
 	MMyLib::INITIALIZE_LOG(argv[0]);
 	if(argc != 2)
 		cout<< "need id"<< endl;
-	
+
 	int t_iConditionId = atoi(argv[1]);
 	//获取id
 	LOG_INFO<< t_iConditionId<< " kkk"<< endl;
+	std::cout<< t_iConditionId<< " kkk"<< std::endl;
 	//解析xml
 	//执行程序
 	return 0;
