0c1d9f712f5122c219908c4a4a3ceb6631da7332
chenhuidong <chdyczx@live.com>
2016-10-05 23:53:31 +0800

    MyFrame

--- MyCpp/MyPro/MyWeb/MyFrame.cpp
+++ MyCpp/MyPro/MyWeb/MyFrame.cpp
@@ -1,5 +1,6 @@
 #include "MyStdAfx.h"
 #include "MyRedis.h"
+#include "MyConf.h"
 using namespace MMyLib;
 
 int main(int argc, char *argv[])
@@ -14,10 +15,17 @@ int main(int argc, char *argv[])
 	cout<< "begin task "<< t_iConditionId<< "."<< endl;
 
 	reply t_oReply = g_pRedisConn->run(command("GET")<< (const char*)argv[1]);
-	LOG_INFO<< "XML filename is: "<< t_oReply.str()<< " "<< t_oReply.type()<< endl;
+	//LOG_INFO<< "XML filename is: "<< t_oReply.str()<< " "<< t_oReply.type()<< endl;
+	LOG_INFO<< "XML filename is: "<< t_oReply.str()<< endl;
+	const char* t_sFileName = t_oReply.str();
 
 	//解析xml
-	
+	IMyConf t_oMyConf;
+	ptree* t_pPt = t_oMyConf.ReadFile(IMyConf::XML, t_sFileName);
+	string t_sCurLib = t_pPt->get<string>("conf.curlib");
+	string t_sCurInterface = t_pPt->get<string>("conf.curinterface");
+
+	LOG_INFO<< "current lib is "<< t_sCurLib<< ". current interface is "<< t_sCurInterface<< "."<< endl;
 	//执行程序
 	return 0;
 }
