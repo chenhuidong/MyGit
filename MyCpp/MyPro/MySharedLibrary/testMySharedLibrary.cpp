6736f4e72018c9cfddf56c78f1e18b311131b3a7
chenhuidong <chdyczx@live.com>
2016-10-08 12:23:35 +0800

    test

--- MyCpp/MyPro/MySharedLibrary/testMySharedLibrary.cpp
+++ MyCpp/MyPro/MySharedLibrary/testMySharedLibrary.cpp
@@ -2,6 +2,7 @@
 
 int main(int argc, char** argv)
 {
+	MMyLib::INITIALIZE_LOG(argv[0]);
 	MMyLib::IMySharedLibrary t_oProvider("/home/chenhuidong/MyProgram/MyGit/MyCpp/MyPro/MySharedLibrary/libMySharedLibraryProvider");
 	t_oProvider.ExecFunc("hello");
 	AbstractPlugin* t_pPluginA = t_oProvider.CreateClass("PluginA");
