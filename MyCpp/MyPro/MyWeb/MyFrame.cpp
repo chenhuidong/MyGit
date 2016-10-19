08b6b0fd950eef778cecc9ebfad144d9b1babe0e
chenhuidong <chdyczx@live.com>
2016-10-19 19:55:29 +0800

    MyTask

--- MyCpp/MyPro/MyWeb/MyFrame.cpp
+++ MyCpp/MyPro/MyWeb/MyFrame.cpp
@@ -1,57 +1,39 @@
 #include "MyFrame.h"
+using namespace MMyLib;
 
-MMyLib::IMyFrame::IMyFrame()
+int main(int argc, char *argv[])
 {
-}
-
-MMyLib::IMyFrame::~IMyFrame()
-{
-}
-
-string& MMyLib::IMyFrame::GetXMLPath(const char* in_sConditionId)
-{
-	reply t_oReply = MMyLib::g_pRedisConn->run(command("GET")<< in_sConditionId);
-	LOG_INFO<< "XML filename is: "<< t_oReply.str()<< "."<< endl;
-	m_sXMLPath = t_oReply.str();
-	if(0 == m_sXMLPath.length())
+	MMyLib::INITIALIZE_LOG(argv[0]);
+	if(argc != 2)
 	{
-		LOG_ERROR<< "Condition id "<< in_sConditionId<< " XML path does not exist."<< endl;
-		throw XMLPathNoRecordException("xml path from redis no exist.");
+		LOG_ERROR<< "Usage: ./MyFrame taskid."<< endl;
+		return SDL_FAILED;
 	}
-	return m_sXMLPath;
-}
 
-ptree* MMyLib::IMyFrame::ParseXML()
-{
-	return m_oMyConf.ReadFile(IMyConf::XML, m_sXMLPath.c_str());
-}
-
-int MMyLib::IMyFrame::BeginNewTask(const char* in_sConditionId)
-{
-	int t_iReturn = SDL_OK;
-	//reply t_oReply = g_pRedisConn->run(command("GET")<< (const char*)argv[1]);
-	//LOG_INFO<< "XML filename is: "<< t_oReply.str()<< "."<< endl;
-	//string t_sFileName = t_oReply.str();
-	
-	GetXMLPath(in_sConditionId);
-
-	//解析xml
-	//IMyConf t_oMyConf;
-	//ptree* t_pPt = t_oMyConf.ReadFile(IMyConf::XML, t_sFileName.c_str());
-	ptree* t_pPt = ParseXML();
-	string t_sCurLib = t_pPt->get<string>("conf.curlib");
-	string t_sCurInterface = t_pPt->get<string>("conf.curinterface");
-
-	LOG_INFO<< "Current lib is "<< t_sCurLib<< ". Current interface is "<< t_sCurInterface<< "."<< endl;
-	
-	//执行程序
-	IMySharedLibrary t_oProvider(t_sCurLib);
-	do
+	/*
+	int t_pid = fork();	
+	if(t_pid < 0)
 	{
-		t_iReturn = t_oProvider.ExecFunc(t_sCurInterface);
-		LOG_INFO<< "Return code is "<< t_iReturn<< "."<< endl;
-		sleep(1);
-	}while(t_iReturn == SDL_OK);
+		LOG_ERROR<< "Fork error"<< endl;
+		return SDL_FAILED;
+	}
+	else if(0 == t_pid)
+	{
+		*/
+	int t_iConditionId = atoi(argv[1]);
+		//获取id
+	LOG_INFO<< "Begin task "<< t_iConditionId<< "."<< endl;
 
-	return t_iReturn;
+	LOG_INFO<< "Begin a new task."<< endl;
+	IMyFrame t_oMyFrame;
+	t_oMyFrame.BeginNewTask(argv[1]);
+	/*
+	}
+	else
+	{
+		LOG_INFO<< "Continue my task."<< endl;
+
+	}
+	*/
+	return SDL_OK;
 }
