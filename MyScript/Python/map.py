a90619d7b1e0873fc761be94e8cd4d5757911f68
chenhuidong <chdyczx@live.com>
2017-02-10 17:21:02 +0800

    map.py

--- MyScript/Python/map.py
+++ MyScript/Python/map.py
@@ -7,3 +7,13 @@ def f(x):
 print map(f, [1, 2, 3, 4, 5, 6, 7, 8, 9])
 
 print map(str, [1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+def add(x, y):
+	return x + y
+
+print reduce(add, [1, 3, 5, 7, 9])
+
+def fn(x, y):
+	return x * 10 + y
+
+reduce(fn, [1, 3, 5, 7, 9])
