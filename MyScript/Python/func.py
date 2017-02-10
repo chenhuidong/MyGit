ac852d11eb66ab93cc1c23e36b2a690398ffa2e0
chenhuidong <chdyczx@live.com>
2017-02-10 09:25:03 +0800

    func

--- MyScript/Python/func.py
+++ MyScript/Python/func.py
@@ -33,3 +33,14 @@ def calc(*numbers):
 
 nums = [1, 2, 3]
 print calc(*nums)
+
+def func(a, b, c=0, *args, **kw):
+    print 'a =', a, 'b =', b, 'c =', c, 'args =', args, 'kw =', kw
+print func(1, 2)
+print func(1, 2, c=3)
+print func(1, 2, 3, 'a', 'b')
+print func(1, 2, 3, 'a', 'b', x=99)   
+
+args = (1, 2, 3, 4)
+kw = {'x': 99}
+print func(*args, **kw)
