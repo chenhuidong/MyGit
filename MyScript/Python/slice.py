9189e6782015a7abbe9bcf367992fddba2b8d71e
chenhuidong <chdyczx@live.com>
2017-02-10 10:08:13 +0800

    slice

--- MyScript/Python/slice.py
+++ MyScript/Python/slice.py
@@ -2,12 +2,12 @@
 # -*- coding: utf-8 -*-
 
 L = ['Michael', 'Sarah', 'Tracy', 'Bob', 'Jack']
-L[0:3]
-L[:3]
-L[1:3]
-L[-2:]
-L = range(100)
-L[:10:2]
-L[::5]
-(0, 1, 2, 3, 4, 5)[:3]
- 'ABCDEFG'[::2]
+print L[0:3]
+print L[:3]
+print L[1:3]
+print L[-2:]
+print L = range(100)
+print L[:10:2]
+print L[::5]
+print (0, 1, 2, 3, 4, 5)[:3]
+print 'ABCDEFG'[::2]
