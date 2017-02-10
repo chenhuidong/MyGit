a49fa758c05cb23bf65e1e18772630e6104b2b5f
chenhuidong <chdyczx@live.com>
2017-02-10 16:48:05 +0800

    generator

---
+++ MyScript/Python/generator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+g = (x * x for x in range(10))
+g.next()
+
+for n in g:
+	print n
+
+def fib(max):
+    n, a, b = 0, 0, 1
+    while n < max:
+        yield b
+        a, b = b, a + b
+        n = n + 1
+
+print fib(6)
