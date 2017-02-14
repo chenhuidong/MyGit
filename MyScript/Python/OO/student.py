c1025574e56ba61c9212fadc6ae9a9004401e9e1
chenhuidong <chdyczx@live.com>
2017-02-14 17:23:30 +0800

    stu

--- MyScript/Python/OO/student.py
+++ MyScript/Python/OO/student.py
@@ -10,7 +10,15 @@ class Student(object):
     def print_score(self):
         print '%s: %s' % (self.name, self.score)
 
+    def get_name(self):
+        return self.__name
+
+    def get_score(self):
+        return self.__score
+
 bart = Student('Bart Simpson', 59)
 lisa = Student('Lisa Simpson', 87)
 bart.print_score()
 lisa.print_score()
+
+print bart.get_name()
