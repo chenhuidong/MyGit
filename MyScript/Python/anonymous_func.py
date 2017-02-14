121b64e8f818b01801873c20080285aa716d2802
chenhuidong <chdyczx@live.com>
2017-02-14 16:31:47 +0800

    py

---
+++ MyScript/Python/anonymous_func.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+print map(lambda x: x * x, [1, 2, 3, 4, 5, 6, 7, 8, 9])
---
+++ MyScript/Python/decorator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def log(func):
+    def wrapper(*args, **kw):
+        print 'call %s():' % func.__name__
+        return func(*args, **kw)
+    return wrapper
+
+@log
+def now():
+    print '2013-12-25'
+
+print now()
