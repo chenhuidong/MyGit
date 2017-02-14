24f44ab1b058f0ead1d948ff0002650d4d1549e2
chenhuidong <chdyczx@live.com>
2017-02-14 16:15:33 +0800

    lazy

--- MyScript/Python/lazy_func.py
+++ MyScript/Python/lazy_func.py
@@ -11,19 +11,20 @@ def lazy_sum(*args):
     return sum
 
 f = lazy_sum(1, 3, 5, 7, 9)
-'''
+
 print f
 print f()
+'''
 
 def count():
-    fs = []
-    for i in range(1, 4):
-        def f(j):
-            def g():
-                return j*j
-            return g
-       fs.append(f(i))
-    return fs
+	fs = []
+	for i in range(1, 4):
+		def f(j):
+			def g():
+			return j*j
+		return g
+		fs.append(f(i))
+	return fs
 
 f1, f2, f3 = count()
 f1()
