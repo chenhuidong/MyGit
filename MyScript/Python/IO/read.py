227f6feab74caca573348c9fe2d81a5c65956b3a
chenhuidong <chdyczx@live.com>
2017-02-15 14:13:37 +0800

    read

--- MyScript/Python/IO/read.py
+++ MyScript/Python/IO/read.py
@@ -2,4 +2,6 @@
 # -*- coding: utf-8 -*-
 
 with open('/home/chenhuidong/MyProgram/MyGithub/MyGit/MyScript/Python/IO/test.txt', 'r') as f:
-    print f.read(3)
+    #print f.read(3)
+    for line in f.readlines():
+    	print(line.strip())
