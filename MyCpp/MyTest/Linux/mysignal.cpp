1e517433672d1fbbdcc0293365449372b48a21d2
chenhuidong <chdyczx@live.com>
2017-05-15 16:59:13 +0800

    mysignal

--- MyCpp/MyTest/Linux/mysignal.cpp
+++ MyCpp/MyTest/Linux/mysignal.cpp
@@ -3,7 +3,14 @@
 
 #define SIG_RECVDATA __SIGRTMIN+10
 
+static void my_op(int)
+{
+	cout<< "my op"<< endl;
+}
+
 int main(int argc, char* argv[])
 {
+	signal(SIG_RECVDATA, my_op);
+	sleep(20);
 	return 0;
 }
