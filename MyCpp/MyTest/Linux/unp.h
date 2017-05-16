9c1c49cac1524483eb12ae5db5e36fab8cceea14
chenhuidong <chdyczx@live.com>
2017-05-16 15:31:53 +0800

    unp

--- MyCpp/MyTest/Linux/unp.h
+++ MyCpp/MyTest/Linux/unp.h
@@ -258,32 +258,6 @@ struct addrinfo {
   struct addrinfo	*ai_next;	/* next structure in linked list */
 };
 
-			/* following for getaddrinfo() */
-#define	AI_PASSIVE		 1	/* socket is intended for bind() + listen() */
-#define	AI_CANONNAME	 2	/* return canonical name */
-
-			/* following for getnameinfo() */
-#define	NI_MAXHOST	  1025	/* max hostname returned */
-#define	NI_MAXSERV	    32	/* max service name returned */
-
-#define	NI_NOFQDN	     1	/* do not return FQDN */
-#define	NI_NUMERICHOST   2	/* return numeric form of hostname */
-#define	NI_NAMEREQD	     4	/* return error if hostname not found */
-#define	NI_NUMERICSERV   8	/* return numeric form of service name */
-#define	NI_DGRAM	    16	/* datagram service for getservbyname() */
-
-			/* error returns */
-#define	EAI_ADDRFAMILY	 1	/* address family for host not supported */
-#define	EAI_AGAIN		 2	/* temporary failure in name resolution */
-#define	EAI_BADFLAGS	 3	/* invalid value for ai_flags */
-#define	EAI_FAIL		 4	/* non-recoverable failure in name resolution */
-#define	EAI_FAMILY		 5	/* ai_family not supported */
-#define	EAI_MEMORY		 6	/* memory allocation failure */
-#define	EAI_NODATA		 7	/* no address associated with host */
-#define	EAI_NONAME		 8	/* host nor service provided, or not known */
-#define	EAI_SERVICE		 9	/* service not supported for ai_socktype */
-#define	EAI_SOCKTYPE	10	/* ai_socktype not supported */
-#define	EAI_SYSTEM		11	/* system error returned in errno */
 #endif
 
 #ifndef	HAVE_IF_NAMEINDEX_STRUCT
