9902a176b9fe2ed473f9daaa57418a2f54f57fd2
chenhuidong <chdyczx@live.com>
2017-06-06 14:47:52 +0800

    main

--- MyCpp/MyTest/MyAI/MyPoker/main.cpp
+++ MyCpp/MyTest/MyAI/MyPoker/main.cpp
@@ -3,8 +3,29 @@
 
 using namespace mypoker;
 
+static void sig_handler(int)
+{
+	pid_t t_iWpid;
+	t_iWpid = waitpid(-1, &t_iStat, WNOHANG);
+	if(t_iWpid == 0)
+	{
+		printf("The child process has not exited \n");  
+		sleep(1);  
+	}
+	else if(t_iWpid > 0)
+	{
+		printf("child %d terminated\n", t_iWpid);
+	}
+}
+
 int main(int argc, char** argv)
 {
+	if(signal(SIGCHLD, sig_handler) == SIG_ERR)  
+    {  
+        printf("signal error : %s\n", strerror(errno));  
+        return 1;
+    } 
+
 	pid_t t_iPid, t_iWpid;
 	int   t_iStat; 
 
@@ -14,17 +35,18 @@ int main(int argc, char** argv)
 		if(t_iPid < 0)
 		{
 			cout<< "fork error."<< endl;
-			exit(1);
+			return 1;
 		}
 		else if(t_iPid == 0)
 		{
 			cout<< "child "<< t_iPid<< endl;
 			sleep(10);
-			exit(0);
+			return 0;
 		}
 	}
 
 	cout<< "parent"<< endl;
+	/*
 	do
 	{ 
 		t_iWpid = waitpid(-1, &t_iStat, WNOHANG);
@@ -37,7 +59,7 @@ int main(int argc, char** argv)
 		{
 			printf("child %d terminated\n", getpid());
 		}
-		/*
+		
 		else
 		{
 			if (WIFEXITED(t_iStat))  
@@ -47,8 +69,8 @@ int main(int argc, char** argv)
 				printf("Child %d terminated due to signal %d znot caught\n",  
 					t_iWpid, WTERMSIG(t_iStat));  
 		}
-		*/
+		
 	}while(t_iWpid >= 0);
-
+*/
 	return 0;
 }
