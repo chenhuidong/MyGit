390b440662f0d35b045d4fdb30cc7cec81c24118
chenhuidong <chdyczx@live.com>
2017-07-13 10:22:06 +0800

    ctl

--- MyCpp/Linux/IPC/SystemV/MQ/ctl.cpp
+++ MyCpp/Linux/IPC/SystemV/MQ/ctl.cpp
@@ -1,8 +1,16 @@
-#include <iostream>  
-#include <sys/ipc.h>  
-#include <sys/msg.h> 
-#include <sys/types.h>	 
-#include <cstring>  
+#include	<sys/types.h>	/* basic system data types */
+#include	<sys/time.h>	/* timeval{} for select() */
+#include	<time.h>		/* timespec{} for pselect() */
+#include	<errno.h>
+#include	<fcntl.h>		/* for nonblocking */
+#include	<limits.h>		/* PIPE_BUF */
+#include	<signal.h>
+#include	<stdio.h>
+#include	<stdlib.h>
+#include	<string.h>
+#include	<sys/stat.h>	/* for S_xxx file mode constants */
+#include	<unistd.h>
+#include	<sys/wait.h> 
 using namespace std;  
   
 struct msgbuff  
