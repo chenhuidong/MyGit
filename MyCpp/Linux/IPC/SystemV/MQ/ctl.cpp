9e18410433d3ab5f3f90173873c5528df8596ffb
chenhuidong <chdyczx@live.com>
2017-07-13 10:14:28 +0800

    ctl

--- MyCpp/Linux/IPC/SystemV/MQ/ctl.cpp
+++ MyCpp/Linux/IPC/SystemV/MQ/ctl.cpp
@@ -22,7 +22,7 @@ int main(int argc, char **argv)
 	msqid = msgget(IPC_PRIVATE, SVMSG_MODE | IPC_CREAT);
 	buf.mtype = 1;
 	buf.mtext[0] = 1;
-	msgsend(msqid, &buf, 1, 0);
+	msgsnd(msqid, &buf, 1, 0);
 	msgctl(msqid, IPC_STAT, &info);
 	printf("read-write:%03o, cbytes = %lu, qnum = %lu, qbytes = %lu\n",
 		info.msg_perm.mode & 0777, (ulong_t)info.msg_cbytes,
