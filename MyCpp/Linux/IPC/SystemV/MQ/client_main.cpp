4f22ff1dc241827c303542f0917b3bd415cefbe5
chenhuidong <chdyczx@live.com>
2017-07-13 16:51:04 +0800

    MQ

--- MyCpp/Linux/IPC/SystemV/MQ/client_main.cpp
+++ MyCpp/Linux/IPC/SystemV/MQ/client_main.cpp
@@ -8,17 +8,17 @@ client(int readfd, int writefd)
 	char	buff[MAXLINE];
 
 		/* 4read pathname */
-	Fgets(buff, MAXLINE, stdin);
+	fgets(buff, MAXLINE, stdin);
 	len = strlen(buff);		/* fgets() guarantees null byte at end */
 	if (buff[len-1] == '\n')
 		len--;				/* delete newline from fgets() */
 
 		/* 4write pathname to IPC channel */
-	Write(writefd, buff, len);
+	write(writefd, buff, len);
 
 		/* 4read from IPC, write to standard output */
-	while ( (n = Read(readfd, buff, MAXLINE)) > 0)
-		Write(STDOUT_FILENO, buff, n);
+	while ( (n = read(readfd, buff, MAXLINE)) > 0)
+		write(STDOUT_FILENO, buff, n);
 }
 
 int
@@ -27,14 +27,14 @@ main(int argc, char **argv)
 	int		readid, writeid;
 
 		/* 4assumes server has created the queues */
-	writeid = Msgget(MQ_KEY1, 0);
-	readid = Msgget(MQ_KEY2, 0);
+	writeid = msgget(MQ_KEY1, 0);
+	readid = msgget(MQ_KEY2, 0);
 
 	client(readid, writeid);
 
 		/* 4now we can delete the queues */
-	Msgctl(readid, IPC_RMID, NULL);
-	Msgctl(writeid, IPC_RMID, NULL);
+	msgctl(readid, IPC_RMID, NULL);
+	msgctl(writeid, IPC_RMID, NULL);
 
 	exit(0);
 }
--- MyCpp/Linux/IPC/SystemV/MQ/server_main.cpp
+++ MyCpp/Linux/IPC/SystemV/MQ/server_main.cpp
@@ -8,7 +8,7 @@ server(int readfd, int writefd)
 	char	buff[MAXLINE+1];
 
 		/* 4read pathname from IPC channel */
-	if ( (n = Read(readfd, buff, MAXLINE)) == 0)
+	if ( (n = read(readfd, buff, MAXLINE)) == 0)
 		err_quit("end-of-file while reading pathname");
 	buff[n] = '\0';		/* null terminate pathname */
 
@@ -17,13 +17,13 @@ server(int readfd, int writefd)
 		snprintf(buff + n, sizeof(buff) - n, ": can't open, %s\n",
 				 strerror(errno));
 		n = strlen(buff);
-		Write(writefd, buff, n);
+		write(writefd, buff, n);
 
 	} else {
 			/* 4open succeeded: copy file to IPC channel */
-		while ( (n = Read(fd, buff, MAXLINE)) > 0)
-			Write(writefd, buff, n);
-		Close(fd);
+		while ( (n = read(fd, buff, MAXLINE)) > 0)
+			write(writefd, buff, n);
+		close(fd);
 	}
 }
 
@@ -32,8 +32,8 @@ main(int argc, char **argv)
 {
 	int		readid, writeid;
 
-	readid = Msgget(MQ_KEY1, SVMSG_MODE | IPC_CREAT);
-	writeid = Msgget(MQ_KEY2, SVMSG_MODE | IPC_CREAT);
+	readid = msgget(MQ_KEY1, SVMSG_MODE | IPC_CREAT);
+	writeid = msgget(MQ_KEY2, SVMSG_MODE | IPC_CREAT);
 
 	server(readid, writeid);
 
