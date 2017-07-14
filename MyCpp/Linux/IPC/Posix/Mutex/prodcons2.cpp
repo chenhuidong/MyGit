9170bd285ff662adc977dd842fd6705a20728ec0
chenhuidong <chdyczx@live.com>
2017-07-14 15:13:00 +0800

    prodcons2

--- MyCpp/Linux/IPC/Posix/Mutex/prodcons2.cpp
+++ MyCpp/Linux/IPC/Posix/Mutex/prodcons2.cpp
@@ -20,26 +20,27 @@ main(int argc, char **argv)
 	pthread_t	tid_produce[MAXNTHREADS], tid_consume;
 
 	if (argc != 3)
-		err_quit("usage: prodcons2 <#items> <#threads>");
+		printf("usage: prodcons2 <#items> <#threads>");
 	nitems = min(atoi(argv[1]), MAXNITEMS);
 	nthreads = min(atoi(argv[2]), MAXNTHREADS);
 
-	Set_concurrency(nthreads);
+	//Set_concurrency(nthreads);
+	pthread_setconcurrency(nthreads);
 		/* 4start all the producer threads */
 	for (i = 0; i < nthreads; i++) {
 		count[i] = 0;
-		Pthread_create(&tid_produce[i], NULL, produce, &count[i]);
+		pthread_create(&tid_produce[i], NULL, produce, &count[i]);
 	}
 
 		/* 4wait for all the producer threads */
 	for (i = 0; i < nthreads; i++) {
-		Pthread_join(tid_produce[i], NULL);
+		pthread_join(tid_produce[i], NULL);
 		printf("count[%d] = %d\n", i, count[i]);	
 	}
 
 		/* 4start, then wait for the consumer thread */
-	Pthread_create(&tid_consume, NULL, consume, NULL);
-	Pthread_join(tid_consume, NULL);
+	pthread_create(&tid_consume, NULL, consume, NULL);
+	pthread_join(tid_consume, NULL);
 
 	exit(0);
 }
@@ -50,15 +51,15 @@ void *
 produce(void *arg)
 {
 	for ( ; ; ) {
-		Pthread_mutex_lock(&shared.mutex);
+		pthread_mutex_lock(&shared.mutex);
 		if (shared.nput >= nitems) {
-			Pthread_mutex_unlock(&shared.mutex);
+			pthread_mutex_unlock(&shared.mutex);
 			return(NULL);		/* array is full, we're done */
 		}
 		shared.buff[shared.nput] = shared.nval;
 		shared.nput++;
 		shared.nval++;
-		Pthread_mutex_unlock(&shared.mutex);
+		pthread_mutex_unlock(&shared.mutex);
 		*((int *) arg) += 1;
 	}
 }
