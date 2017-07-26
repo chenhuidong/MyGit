fbf4f7f958ff6d4d3b9f42cfb914d5eef2dea3e4
chenhuidong <chdyczx@live.com>
2017-07-26 15:43:05 +0800

    hw

--- MyCpp/MyTest/ZeroMQ/RequestReply/hwclient.c
+++ MyCpp/MyTest/ZeroMQ/RequestReply/hwclient.c
@@ -1,38 +1,32 @@
 //
-//  Hello World 客户端
-//  连接REQ套接字至 tcp://localhost:5555
-//  发送Hello给服务端，并接收World
+//  Hello World client in C++
+//  Connects REQ socket to tcp://localhost:5555
+//  Sends "Hello" to server, expects "World" back
 //
-#include <zmq.h>
-#include <string.h>
-#include <stdio.h>
-#include <unistd.h>
- 
-int main (void)
+#include <zmq.hpp>
+#include <string>
+#include <iostream>
+
+int main ()
 {
-    void *context = zmq_init (1);
- 
-    //  连接至服务端的套接字
-    printf ("正在连接至hello world服务端...\n");
-    void *requester = zmq_socket (context, ZMQ_REQ);
-    zmq_connect (requester, "tcp://127.0.0.1:54321");
- 
-    int request_nbr;
-    for (request_nbr = 0; request_nbr != 10; request_nbr++) {
-        zmq_msg_t request;
-        zmq_msg_init_size (&request, 5);
-        memcpy (zmq_msg_data (&request), "Hello", 5);
-        printf ("正在发送 Hello %d...\n", request_nbr);
-        zmq_send (requester, &request, sizeof(request), 0);
-        zmq_msg_close (&request);
- 
-        zmq_msg_t reply;
-        zmq_msg_init (&reply);
-        zmq_recv (requester, &reply, 0);
-        printf ("接收到 World %d\n", request_nbr);
-        zmq_msg_close (&reply);
+    //  Prepare our context and socket
+    zmq::context_t context (1);
+    zmq::socket_t socket (context, ZMQ_REQ);
+
+    std::cout << "Connecting to hello world server..." << std::endl;
+    socket.connect ("tcp://127.0.0.1:54321");
+
+    //  Do 10 requests, waiting each time for a response
+    for (int request_nbr = 0; request_nbr != 10; request_nbr++) {
+        zmq::message_t request (5);
+        memcpy (request.data (), "Hello", 5);
+        std::cout << "Sending Hello " << request_nbr << "..." << std::endl;
+        socket.send (request);
+
+        //  Get the reply.
+        zmq::message_t reply;
+        socket.recv (&reply);
+        std::cout << "Received World " << request_nbr << std::endl;
     }
-    zmq_close (requester);
-    zmq_term (context);
     return 0;
 }
