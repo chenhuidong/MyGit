edd8c047c5f2de21ec7de15ef08afe825333eded
chenhuidong <chdyczx@live.com>
2017-08-01 11:56:43 +0800

    index

--- MyScript/MyWeb/index.cpp
+++ MyScript/MyWeb/index.cpp
@@ -13,6 +13,8 @@ int main ()
    cout << "<body>\n";
    cout << "<h1>MessiLinux.</h1><br><br><br>\n";
    cout << "<p>Welcome, here is a free linux for you.</p>\n";
+   cout << "<p>list of installed software:</p>\n";
+   cout << "<p>&emsp boost</p>\n";
    cout << "</body>\n";  
    cout << "</html>\n";   
    return 0;  
