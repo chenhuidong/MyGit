1d41b2c8dc3c132d316653efd735d9846d5ba123
chenhuidong <chdyczx@live.com>
2017-08-01 14:53:52 +0800

    signed

--- MyScript/MyWeb/signupback.cpp
+++ MyScript/MyWeb/signupback.cpp
@@ -10,7 +10,12 @@ int main ()
     cout << "<body>\n"; 
     if( getenv("QUERY_STRING" ) )
     {
-        cout<< getenv("QUERY_STRING" );
+        char *t_sSignUp = getenv("QUERY_STRING" );
+        char t_sUsername[256] = {0};
+        char t_sPassword[256] = {0};
+        sscanf(t_sSignUp,"username=%s&password=%s",&t_sUsername,&t_sPassword);
+        cout<< t_sUsername<< endl;
+        cout<< t_sPassword<< endl;
     }
     cout << "</body>\n";  
     cout << "</html>\n"; 
