d3d75b580b8aaf7eb7bc4ce965fde7aa3c78718c
chenhuidong <chdyczx@live.com>
2017-08-01 14:38:47 +0800

    signed

--- MyScript/MyWeb/signupback.cpp
+++ MyScript/MyWeb/signupback.cpp
@@ -5,10 +5,14 @@ using namespace std;
    
 int main ()  
 {
-    //cout<< "Content-type:text/html\n\n";
+    cout << "Content-type:text/html\r\n\r\n";  
+    cout << "<html>\n"; 
+    cout << "<body>\n"; 
     if( getenv("QUERY_STRING" ) )
     {
         cout<< getenv("QUERY_STRING" );
     }
+    cout << "</body>\n";  
+    cout << "</html>\n"; 
     return 0;
 }
