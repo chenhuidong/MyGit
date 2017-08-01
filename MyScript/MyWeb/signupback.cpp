9bc2ca305dd5e12465c711442051dcae7e808a13
chenhuidong <chdyczx@live.com>
2017-08-01 15:48:46 +0800

    signed

--- MyScript/MyWeb/signupback.cpp
+++ MyScript/MyWeb/signupback.cpp
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <iostream>   
 using namespace std;  
+
+
    
 int main ()  
 {
@@ -14,10 +16,10 @@ int main ()
         char t_sUsername[256] = {0};
         char t_sPassword[256] = {0};
         sscanf(t_sSignUp,"username=%[^&]&password=%s",t_sUsername,t_sPassword);
-        //cout<< t_sUsername<< "<br>"<< endl;
-        //cout<< t_sPassword<< "<br>"<< endl;
+
         if(t_sUsername&&t_sPassword)
         {
+
             cout<< "hello,"<< t_sUsername<<". you have signed success." << "<br><br>\n"<< endl;
             cout<< "ssh config:"<< "<br>\n"<< endl;
             cout<< "IP:         124.161.110.68"<< "<br>\n"<< endl;
