8ca92fee6fe43778b1a7f32999c48542da7f52e8
chenhuidong <chdyczx@live.com>
2017-08-01 11:05:46 +0800

    index

--- MyScript/MyWeb/index.cpp
+++ MyScript/MyWeb/index.cpp
@@ -7,10 +7,12 @@ int main ()
    cout << "Content-type:text/html\r\n\r\n";  
    cout << "<html>\n";  
    cout << "<head>\n";  
-   cout << "<title>Hello World - First CGI Program</title>\n";  
+   cout << "<title>MessiLinux</title>\n";  
    cout << "</head>\n";  
-   cout << "<body>\n";  
-   cout << "<h2>Hello World! This is my first CGI program</h2>\n";  
+   cout << "<body>\n";
+   cout << "\n\n\n";
+   cout << "<h1>Messi Linux.</h1>\n";
+   cout << "<p>Welcome, here is a free linux for you.</p>\n";  
    cout << "</body>\n";  
    cout << "</html>\n";  
      
