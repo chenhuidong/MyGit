716f9671d773cbc24f6e228d9a2f1a196cc35d4d
chenhuidong <chdyczx@live.com>
2017-08-01 13:51:53 +0800

    signedup

---
+++ MyScript/MyWeb/signup.cpp
@@ -0,0 +1,21 @@
+#include <iostream>   
+using namespace std;  
+   
+int main ()  
+{
+	cout << "Content-type:text/html\r\n\r\n";  
+	cout << "<html>\n";  
+	cout << "<head>\n"; 
+	cout << "<br>"; 
+	cout << "<title>Messi Linux</title>\n";  
+	cout << "</head>\n";  
+	cout << "<body>\n";
+	cout << "<h1>Messi Linux.</h1>\n";
+	cout << "<form>\n";
+	cout << "username: <input type=\"text\" name=\"username\"><br>\n";
+	cout << "password: <input type=\"password\" name=\"password\"><br>\n";
+	cout << "</form>\n";
+	cout << "</body>\n";  
+	cout << "</html>\n";  
+	return 0;  
+}
