1f23fba67a53c32aad527ea88342cfa75711c5d8
chenhuidong <chdyczx@live.com>
2017-08-01 11:22:32 +0800

    index

--- MyScript/MyWeb/index.cpp
+++ MyScript/MyWeb/index.cpp
@@ -6,13 +6,13 @@ int main ()
       
    cout << "Content-type:text/html\r\n\r\n";  
    cout << "<html>\n";  
-   cout << "<head>\n";  
+   cout << "<head>\n"; 
+   cout << "<br><br><br>"; 
    cout << "<title>MessiLinux</title>\n";  
    cout << "</head>\n";  
    cout << "<body>\n";
-   cout << "\n\n\n";
-   cout << "<h1>Messi Linux.</h1>\n";
-   cout << "<p>Welcome, here is a free linux for you.</p>\n";  
+   cout << "<h1>MessiLinux.</h1><br><br><br>\n";
+   cout << "<p>Welcome, here is a free linux for you.</p>\n";
    cout << "</body>\n";  
    cout << "</html>\n";   
    return 0;  
