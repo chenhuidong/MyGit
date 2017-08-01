f1cd597081414149e8fcb1b9aab2a7b259ee77f5
chenhuidong <chdyczx@live.com>
2017-08-01 11:59:08 +0800

    index

--- MyScript/MyWeb/index.cpp
+++ MyScript/MyWeb/index.cpp
@@ -7,14 +7,14 @@ int main ()
    cout << "Content-type:text/html\r\n\r\n";  
    cout << "<html>\n";  
    cout << "<head>\n"; 
-   cout << "<br><br><br>"; 
+   cout << "<br>"; 
    cout << "<title>MessiLinux</title>\n";  
    cout << "</head>\n";  
    cout << "<body>\n";
-   cout << "<h1>MessiLinux.</h1><br><br><br>\n";
+   cout << "<h1>MessiLinux.</h1><br>\n";
    cout << "<p>Welcome, here is a free linux for you.</p>\n";
-   cout << "<p>list of installed software:</p>\n";
-   cout << "<p>&emsp boost</p>\n";
+   cout << "<p>list of installed software:\n";
+   cout << "&emsp boost</p>\n";
    cout << "</body>\n";  
    cout << "</html>\n";   
    return 0;  
