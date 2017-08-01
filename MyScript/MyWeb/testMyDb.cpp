e1624d6d4dc79aa4bc77c953abb07028ac31e914
chenhuidong <chdyczx@live.com>
2017-08-01 16:43:00 +0800

    test

--- MyScript/MyWeb/testMyDb.cpp
+++ MyScript/MyWeb/testMyDb.cpp
@@ -4,7 +4,7 @@ int main(int argc, char** argv)
 {
 	MMyLib::INITIALIZE_LOG(argv[0]);
 	MMyLib::MyDb t_oMyDb;
-	t_oMyDb.Initialize((MMyLib::MyDb::DbType)2, "testMyDb.db");
+	t_oMyDb.Initialize((MMyLib::MyDb::DbType)1, "testMyDb.db");
 	t_oMyDb.Uninitialize();
 	return 0;
 }
