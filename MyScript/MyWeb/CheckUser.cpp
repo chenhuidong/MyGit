38aa181bb23f972e2b95e86f871a18001a0b8642
chenhuidong <chdyczx@live.com>
2017-08-04 00:32:32 +0800

    c

--- MyScript/MyWeb/CheckUser.cpp
+++ MyScript/MyWeb/CheckUser.cpp
@@ -67,7 +67,7 @@ int UserCheck::ExistUsername(char* in_sUsername)
     LOG_INFO<< "ExistUsername begin.";
 
     char iSQL[1024] = {0};
-    snprintf(iSQL, sizeof(iSQL), "select m_id, m_username, m_password, m_instance from messi_users where m_username = '%s'", in_sUsername);
+    snprintf(iSQL, sizeof(iSQL), "select m_id, m_username, m_password, m_instance from wizard_users where m_username = '%s'", in_sUsername);
     m_oMyDb.ExecuteSQL(iSQL, m_oUsers);
     
     LOG_INFO<< "ExistUsername end.";
@@ -79,7 +79,7 @@ int UserCheck::ExistUsernamePassword(char* in_sUsername, char* in_sPassword)
     LOG_INFO<< "ExistUsernamePassword begin.";
 
     char iSQL[1024] = {0};
-    snprintf(iSQL, sizeof(iSQL), "select m_id, m_username, m_password, m_instance from messi_users where m_username = '%s' and m_password = '%s'", in_sUsername, in_sPassword);
+    snprintf(iSQL, sizeof(iSQL), "select m_id, m_username, m_password, m_instance from wizard_users where m_username = '%s' and m_password = '%s'", in_sUsername, in_sPassword);
     m_oMyDb.ExecuteSQL(iSQL, m_oUsers);
     
     LOG_INFO<< "ExistUsernamePassword end.";
@@ -96,7 +96,7 @@ int UserCheck::InsertUsers(char* in_sUsername, char* in_sPassword)
 
     char iSQL[1024] = {0};
 
-    snprintf(iSQL, sizeof(iSQL), "insert into messi_users (m_username, m_password, m_instance) values ('%s', '%s', '%s')", in_sUsername, in_sPassword, in_sUsername);
+    snprintf(iSQL, sizeof(iSQL), "insert into wizard_users (m_username, m_password, m_instance) values ('%s', '%s', '%s')", in_sUsername, in_sPassword, in_sUsername);
     m_oMyDb.ExecuteSQL(iSQL, m_oUsers);
 
     snprintf(iSQL, sizeof(iSQL), "create database %s;", in_sUsername);
