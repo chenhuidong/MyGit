fe41e467e6d7c1a0337f36c66091235178768d69
chenhuidong <chdyczx@live.com>
2017-10-26 20:01:45 +0800

    myredis

---
+++ MyCpp/MyPro2/include/MyRedis.h
@@ -0,0 +1,15 @@
+#ifndef __MY_LIB_MY_REDIS_H_
+#define __MY_LIB_MY_REDIS_H_
+
+#include "MyStdAfx.h"
+#include <redis3m/redis3m.hpp>
+#include <iostream>
+
+using namespace redis3m;
+
+namespace MMyLib
+{
+	const connection::ptr_t g_pRedisConn = connection::create(REDISIP, REDISPORT);
+};
+
+#endif
