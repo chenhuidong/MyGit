95896d10d1037b7e40d0ed094c953467436dae59
chenhuidong <chdyczx@live.com>
2017-10-26 19:28:41 +0800

    basefunc

---
+++ MyCpp/MyPro2/include/MyEnum.h
@@ -0,0 +1,23 @@
+#ifndef __MY_LIB_MY_ENUM_H_
+#define __MY_LIB_MY_ENUM_H_
+
+#define MYSIZE 4096
+#define SERVIP "127.0.0.1"
+#define SERVPORT 54321
+#define CLTIP "127.0.0.1"
+#define CLTPORT 54321
+#define REDISIP "127.0.0.1"
+#define REDISPORT 6379
+
+typedef long long int64;
+typedef int int32;
+typedef short int16;
+
+enum 
+{
+	SDL_FAILED = -1,
+	SDL_OK = 0,
+	SDL_ONCE = 1,
+};
+
+#endif
