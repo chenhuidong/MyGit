22d40c32fcb38f0b1d7bc86f02753348ce5903cd
chenhuidong <chdyczx@live.com>
2017-10-26 19:47:41 +0800

    mydatetime

---
+++ MyCpp/MyPro2/src/MyDateTime.cpp
@@ -0,0 +1,37 @@
+#include "MyDateTime.h"
+
+MMyLib::IMyDateTime::IMyDateTime()
+{}
+
+MMyLib::IMyDateTime::~IMyDateTime()
+{}
+
+ptime MMyLib::IMyDateTime::GetLocalTime()
+{
+	return second_clock::local_time();
+}
+
+ptime MMyLib::IMyDateTime::GetLocalTimeMic()
+{
+	return microsec_clock::local_time();
+}
+
+ptime MMyLib::IMyDateTime::GetUTCTime()
+{
+	return second_clock::universal_time();
+}
+
+ptime MMyLib::IMyDateTime::GetUTCTimeMic()
+{
+	return microsec_clock::universal_time();
+}
+
+ptime MMyLib::IMyDateTime::AddDays(const ptime& in_oDateTime, double in_iDays)
+{
+	return in_oDateTime + days(in_iDays);
+}
+
+time_duration MMyLib::IMyDateTime::GetDuration(const ptime& in_oDateTimeLeft,  const ptime& in_oDateTimeRight)
+{
+	return in_oDateTimeLeft - in_oDateTimeRight;
+}
