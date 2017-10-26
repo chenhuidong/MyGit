ad63f0ecaa916e8bd103c7a728978b8ba62bf1d4
chenhuidong <chdyczx@live.com>
2017-10-26 20:13:43 +0800

    sharedlibrary

---
+++ MyCpp/MyPro2/include/MySharedLibrary.h
@@ -0,0 +1,33 @@
+#ifndef __MY_LIB_MY_SHARED_LIBRARY_H_
+#define __MY_LIB_MY_SHARED_LIBRARY_H_
+
+#include "MyStdAfx.h"
+#include "Poco/SharedLibrary.h"
+#include "Poco/ClassLoader.h"
+#include "Poco/Manifest.h"
+#include "testMySharedLibraryProvider.h"
+
+using Poco::SharedLibrary;
+
+namespace MMyLib
+{
+typedef int (*Func)(); 
+typedef Poco::ClassLoader<AbstractPlugin> PluginLoader;
+typedef Poco::Manifest<AbstractPlugin> PluginManifest;
+
+class IMySharedLibrary
+{
+public:
+	IMySharedLibrary(string in_sPathName);
+	virtual ~IMySharedLibrary();
+public:
+	int ExecFunc(string in_sFuncName);
+	AbstractPlugin* CreateClass(string in_sClassName);
+private:
+	string 		  m_sPathName;
+	SharedLibrary m_oLibrary;
+	PluginLoader  m_oLoader;
+};
+};
+
+#endif
---
+++ MyCpp/MyPro2/src/MySharedLibrary.cpp
@@ -0,0 +1,25 @@
+#include "MySharedLibrary.h"
+
+MMyLib::IMySharedLibrary::IMySharedLibrary(string in_sPathName): m_oLibrary(m_sPathName), m_sPathName(in_sPathName.append(SharedLibrary::suffix()))
+{
+	m_oLoader.loadLibrary(m_sPathName);
+}
+
+MMyLib::IMySharedLibrary::~IMySharedLibrary()
+{
+	m_oLibrary.unload();
+	m_oLoader.unloadLibrary(m_sPathName);
+}
+
+int MMyLib::IMySharedLibrary::ExecFunc(string in_sFuncName)
+{
+	Func t_pFunc = (Func) m_oLibrary.getSymbol(in_sFuncName);
+	if(!t_pFunc)
+		throw MMyLib::LoadFuncException("Func name not exist.");
+	t_pFunc();
+}
+
+AbstractPlugin* MMyLib::IMySharedLibrary::CreateClass(string in_sClassName)
+{
+	return m_oLoader.create(in_sClassName);
+}
