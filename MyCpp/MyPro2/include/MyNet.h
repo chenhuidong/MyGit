c8bc89bfc29887c867b24af0fd4e58c2ab3fae99
chenhuidong <chdyczx@live.com>
2017-10-27 15:36:30 +0800

    MyNet

---
+++ MyCpp/MyPro2/include/MyNet.h
@@ -0,0 +1,86 @@
+#ifndef __MY_LIB_MY_NET_H_
+#define __MY_LIB_MY_NET_H_
+
+#include "MyStdAfx.h"
+#include "MyTR.h"
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+using namespace boost;
+using namespace boost::asio;
+
+//#include "student.pb.h"
+
+namespace MMyLib
+{
+typedef boost::shared_ptr<ip::tcp::socket> sock_pt;
+
+class MySessionBase
+{
+public:
+	MySessionBase(boost::asio::io_service& ios);
+	virtual ~MySessionBase();
+
+	virtual void start() = 0;
+	virtual void write_handler(const boost::system::error_code& ec) = 0;
+	virtual void read_handler(const boost::system::error_code& ec, boost::shared_ptr<vector<char> > str) = 0;
+public:
+    sock_pt m_oSocket;
+};
+
+class MyServSession1: public MySessionBase, public boost::enable_shared_from_this<MyServSession1>
+{
+public:
+	MyServSession1(boost::asio::io_service& io_service);
+	virtual ~MyServSession1();
+
+	void start();
+	void write_handler(const boost::system::error_code& ec);
+	void read_handler(const boost::system::error_code& ec, boost::shared_ptr<vector<char> > str);
+};
+
+
+class MyCltSession1: public MySessionBase, public boost::enable_shared_from_this<MyCltSession1>
+{
+public:
+	MyCltSession1(boost::asio::io_service& io_service);
+	virtual ~MyCltSession1();
+
+	void start();
+	void write_handler(const boost::system::error_code& ec);
+	void read_handler(const boost::system::error_code& ec, boost::shared_ptr<vector<char> > str);
+};
+
+class MyServer
+{
+public:
+	MyServer(io_service& in_oIos);
+	virtual ~MyServer();
+
+	void start();
+	template <typename T>
+	void accept_handler(boost::shared_ptr<T> new_session, const boost::system::error_code& ec)
+	{
+		if (ec)
+			return;
+	    new_session->start();
+	    start();		
+	}
+private:
+	boost::asio::io_service& m_oIos;
+  	ip::tcp::acceptor m_oAcceptor;
+};
+
+class MyClient
+{
+public:
+	MyClient(io_service& in_oIos);
+	virtual ~MyClient();
+	void start();
+	void conn_handler(boost::shared_ptr<MyCltSession1> new_session, const boost::system::error_code& ec);
+private:
+	boost::asio::io_service& m_oIos;
+	ip::tcp::endpoint m_oEp;
+};
+
+};
+#endif
---
+++ MyCpp/MyPro2/src/MyNet.cpp
@@ -0,0 +1,124 @@
+#include "MyNet.h"
+
+MMyLib::MySessionBase::MySessionBase(boost::asio::io_service& ios): m_oSocket(new ip::tcp::socket(ios))
+{}
+
+MMyLib::MySessionBase::~MySessionBase()
+{}
+
+//MyServerSession
+MMyLib::MyServSession1::MyServSession1(boost::asio::io_service& ios): MySessionBase(ios)
+{}
+
+MMyLib::MyServSession1::~MyServSession1()
+{}
+
+void MMyLib::MyServSession1::start()
+{
+	LOG_INFO<< "recive from "<< m_oSocket->remote_endpoint().address()<< ":"<< m_oSocket->remote_endpoint().port()<< endl;
+	boost::shared_ptr<vector<char> > str(new vector<char>(100, 0));
+	m_oSocket->async_read_some(buffer(*str), boost::bind(&MyServSession1::read_handler, shared_from_this(), boost::asio::placeholders::error, str));
+}
+
+void MMyLib::MyServSession1::write_handler(const boost::system::error_code& ec)
+{
+	if (ec)
+		return;
+	boost::shared_ptr<vector<char> > str(new vector<char>(100, 0));
+	m_oSocket->async_read_some(buffer(*str), boost::bind(&MMyLib::MyServSession1::read_handler, shared_from_this(), boost::asio::placeholders::error, str));
+}
+
+void MMyLib::MyServSession1::read_handler(const boost::system::error_code& ec, boost::shared_ptr<vector<char> > str)
+{
+	if (ec)
+		return;
+	//LOG_INFO<< &(*str)[0]<< endl;
+	/*lm::helloworld t_ohello;
+	t_ohello.ParseFromString(&(*str)[0]);
+	LOG_INFO<< "id: "<< t_ohello.id()<< " str: "<< t_ohello.str()<< endl;
+	cout<< "id: "<< t_ohello.id()<< " str: "<< t_ohello.str()<< endl;
+	*/
+	m_oSocket->async_write_some(buffer("hello asio"), boost::bind(&MyServSession1::write_handler, shared_from_this(), boost::asio::placeholders::error));
+}
+
+//MyServer
+MMyLib::MyServer::MyServer(io_service& in_oIos): m_oIos(in_oIos), m_oAcceptor(in_oIos, ip::tcp::endpoint(ip::tcp::v4(), SERVPORT))
+{
+	start();
+}
+
+MMyLib::MyServer::~MyServer()
+{}
+
+void MMyLib::MyServer::start()
+{
+	boost::shared_ptr<MMyLib::MyServSession1> new_session = boost::make_shared<MMyLib::MyServSession1>(m_oIos);
+	m_oAcceptor.async_accept(*(new_session->m_oSocket), boost::bind(&MyServer::accept_handler<MMyLib::MyServSession1>, this, new_session, boost::asio::placeholders::error));
+}
+
+/*
+void MMyLib::MyServer::accept_handler(boost::shared_ptr<MyServSession1> new_session, const boost::system::error_code& ec)
+{
+	if (ec)
+		return;
+    new_session->start();
+    start();
+}
+*/
+
+
+//MyCltSession
+MMyLib::MyCltSession1::MyCltSession1(boost::asio::io_service& ios): MySessionBase(ios)
+{}
+
+MMyLib::MyCltSession1::~MyCltSession1()
+{}
+
+void MMyLib::MyCltSession1::start()
+{
+	/*lm::helloworld t_ohello; 
+  	t_ohello.set_id(101); 
+  	t_ohello.set_str("hello my."); 
+  	string t_sMsg;
+	t_ohello.SerializePartialToString(&t_sMsg);*/
+
+	m_oSocket->async_write_some(buffer(t_sMsg), boost::bind(&MyCltSession1::write_handler, shared_from_this(), boost::asio::placeholders::error));
+}
+
+void MMyLib::MyCltSession1::write_handler(const boost::system::error_code& ec)
+{
+	if(ec)
+		return;
+	boost::shared_ptr<vector<char> > str(new vector<char>(100, 0));
+	m_oSocket->async_read_some(buffer(*str), boost::bind(&MyCltSession1::read_handler, shared_from_this(), boost::asio::placeholders::error, str));
+}
+
+void MMyLib::MyCltSession1::read_handler(const boost::system::error_code& ec, boost::shared_ptr<vector<char> > str)
+{
+	if(ec)
+		return;
+	LOG_INFO<< &(*str)[0]<< endl<< endl;
+}
+
+//MyClient
+MMyLib::MyClient::MyClient(io_service& in_oIos): m_oIos(in_oIos), m_oEp(ip::address::from_string(CLTIP), CLTPORT)
+{
+	start();
+}
+
+MMyLib::MyClient::~MyClient()
+{}
+
+void MMyLib::MyClient::start()
+{
+	boost::shared_ptr<MMyLib::MyCltSession1> new_session = boost::make_shared<MMyLib::MyCltSession1>(m_oIos);
+	new_session->m_oSocket->async_connect(m_oEp, boost::bind(&MyClient::conn_handler, this, new_session, boost::asio::placeholders::error));
+}
+
+void MMyLib::MyClient::conn_handler(boost::shared_ptr<MyCltSession1> new_session, const boost::system::error_code& ec)
+{
+	if(ec)
+		return;
+	new_session->start();
+}
+
