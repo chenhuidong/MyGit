82f0e9fd2f8f9171dc8820182e52cf011c732f4d
chenhuidong <chdyczx@live.com>
2017-10-30 12:45:31 +0800

    mylog

--- MyCpp/MyPro2/include/MyLog.h
+++ MyCpp/MyPro2/include/MyLog.h
@@ -12,8 +12,8 @@ namespace MMyLib
 	#define LOG_ERROR pLog->error
 	#define LOG_FATAL pLog->fatal
  
-	int Initialize(const char * in_sFileName = NULL);
-	int Uninitialize();
+	extern int Initialize(const char * in_sFileName = NULL);
+	extern int Uninitialize();
 	static log4cpp::Category* pLog;
 
 };
