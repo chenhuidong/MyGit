6c764af3307056a2fefdfeb4192a600696988545
chenhuidong <chdyczx@live.com>
2017-10-30 14:35:23 +0800

    testMyLog

--- MyCpp/MyPro2/test/testMyLog.cpp
+++ MyCpp/MyPro2/test/testMyLog.cpp
@@ -6,9 +6,9 @@ int main(int argc, char* argv[])
 {
 	std::string initFileName = "MyLog.properties";
 
-	MMyLib::Initialize(initFileName.c_str());
+	Initialize(initFileName.c_str());
 	LOG_WARN("Storm is coming");
 	LOG_INFO("Ready for storm.");
-	MMyLib::Uninitialize();
+	Uninitialize();
 	return 0;
 }
