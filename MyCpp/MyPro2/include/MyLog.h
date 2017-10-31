84c7019a71988e65c140f918f54ce7cc72f685be
chenhuidong <chdyczx@live.com>
2017-10-31 10:57:29 +0800

    testMyLog

--- MyCpp/MyPro2/include/MyLog.h
+++ MyCpp/MyPro2/include/MyLog.h
@@ -12,8 +12,8 @@ namespace MMyLib
 	#define LOG_ERROR pLog->error
 	#define LOG_FATAL pLog->fatal
  
-	extern "C" int Initialize(const char * in_sFileName = NULL);
-	extern "C" int Uninitialize();
+	int InitLog(const char * in_sFileName = NULL);
+	int UninitLog();
 	static log4cpp::Category* pLog;
 
 };
--- MyCpp/MyPro2/test/testMyLog.cpp
+++ MyCpp/MyPro2/test/testMyLog.cpp
@@ -2,13 +2,16 @@
 #include "MyLog.h"
 using namespace MMyLib;
 
+extern int InitLog(const char * in_sFileName = NULL);
+extern int UninitLog();
+
 int main(int argc, char* argv[])
 {
 	std::string initFileName = "MyLog.properties";
 
-	Initialize(initFileName.c_str());
+	InitLog(initFileName.c_str());
 	LOG_WARN("Storm is coming");
 	LOG_INFO("Ready for storm.");
-	Uninitialize();
+	UninitLog();
 	return 0;
 }
