b022ca8134be85b38f82e9028ce7db10b67cdc4b
chenhuidong <chdyczx@live.com>
2017-11-01 14:53:57 +0800

    testMyDb

---
+++ MyCpp/MyPro2/test/testMyDb.cpp
@@ -0,0 +1,13 @@
+#include "MyDb.h"
+using namespace MMyLib;
+
+int main(int argc, char** argv)
+{
+	std::string initFileName = "MyLog.properties";
+	InitLog(initFileName.c_str());
+	MMyLib::MyDb t_oMyDb;
+	t_oMyDb.Initialize((MMyLib::MyDb::DbType)3, "testMyDb.db");
+	t_oMyDb.Uninitialize();
+	UninitLog();
+	return 0;
+}
