14829e39723bda5b89b53f61f117ee1c612d686f
chenhuidong <chdyczx@live.com>
2017-11-01 15:14:36 +0800

    test

--- MyCpp/MyPro2/test/testMyDb.cpp
+++ MyCpp/MyPro2/test/testMyDb.cpp
@@ -7,6 +7,7 @@ int main(int argc, char** argv)
 	InitLog(initFileName.c_str());
 	MMyLib::MyDb t_oMyDb;
 	t_oMyDb.Initialize((MMyLib::MyDb::DbType)0, "testMyDb.db");
+	t_oMyDb.GetSession() << "CREATE TABLE IF NOT EXISTS Employees (Empno int, Name VARCHAR(256), Email VARCHAR(256), ValidFlag interger(1) default 0)", now;
 	t_oMyDb.Uninitialize();
 	UninitLog();
 	return 0;
