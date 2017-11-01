4f0839d43921855d0d0954eaa5e7fdc3435aa6c5
chenhuidong <chdyczx@live.com>
2017-11-01 11:22:47 +0800

    mypro2

--- MyCpp/MyPro2/include/MyLog.h
+++ MyCpp/MyPro2/include/MyLog.h
@@ -1,17 +1,16 @@
 #ifndef __MY_LIB_MY_LOG_H_
 #define __MY_LIB_MY_LOG_H_
 
-
 #include "MyStdAfx.h"
 #include <log4cpp/Category.hh>
 #include <log4cpp/PropertyConfigurator.hh>
 
 namespace MMyLib
 {
-	#define LOG_INFO pLog->info
-	#define LOG_WARN pLog->warn
-	#define LOG_ERROR pLog->error
-	#define LOG_FATAL pLog->fatal
+	#define LOG_INFO MMyLib::pLog->info
+	#define LOG_WARN MMyLib::pLog->warn
+	#define LOG_ERROR MMyLib::pLog->error
+	#define LOG_FATAL MMyLib::pLog->fatal
  
 	int InitLog(const char * in_sInitFileName);
 	int UninitLog();
