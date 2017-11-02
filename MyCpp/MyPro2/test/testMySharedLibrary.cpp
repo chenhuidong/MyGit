4b573fef144b38f70737dd8a077a7bbabd09e65f
chenhuidong <chdyczx@live.com>
2017-11-02 10:21:44 +0800

    test

--- MyCpp/MyPro2/test/testMySharedLibrary.cpp
+++ MyCpp/MyPro2/test/testMySharedLibrary.cpp
@@ -6,7 +6,7 @@ int main(int argc, char** argv)
 	std::string initFileName = "MyLog.properties";
     InitLog(initFileName.c_str());
 	IMySharedLibrary t_oProvider("/home/chenhuidong/MyProgram/MyGithub/MyGit/MyCpp/MyPro2/test/libMySharedLibraryProvider");
-	t_oProvider.ExecFunc("hello");
+	t_oProvider.ExecFunc("hellokitty");
 	AbstractPlugin* t_pPluginA = t_oProvider.CreateClass("PluginA");
 	t_pPluginA->HelloWorld();
 	delete t_pPluginA;	
