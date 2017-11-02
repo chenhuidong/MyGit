04d01e6389b8801da8ba10c1f6b5ca7f232eb9e5
chenhuidong <chdyczx@live.com>
2017-11-02 09:55:58 +0800

    testMyShared

---
+++ MyCpp/MyPro2/test/testMySharedLibrary.cpp
@@ -0,0 +1,14 @@
+#include "MySharedLibrary.h"
+using namespace MMyLib;
+
+int main(int argc, char** argv)
+{
+	std::string initFileName = "MyLog.properties";
+    InitLog(initFileName.c_str());
+	IMySharedLibrary t_oProvider("/home/chenhuidong/MyProgram/MyGit/MyCpp/MyPro2/test/libMySharedLibraryProvider");
+	t_oProvider.ExecFunc("hello");
+	AbstractPlugin* t_pPluginA = t_oProvider.CreateClass("PluginA");
+	t_pPluginA->HelloWorld();
+	delete t_pPluginA;	
+	return 0;
+}
