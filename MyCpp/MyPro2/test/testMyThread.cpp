4f95ad501949f9d8d16e2e184b8605c09e61de5c
chenhuidong <chdyczx@live.com>
2017-11-02 10:49:02 +0800

    thread

---
+++ MyCpp/MyPro2/test/testMyThread.cpp
@@ -0,0 +1,16 @@
+#include "MyThread.h"
+using namespace MMyLib;
+
+void my_func(int x, int y)
+{
+	std::cout<< "thread test. "<< x<< " "<< y<< endl;
+}
+
+int main()
+{
+	int x = 5, k = 4;
+	IMyThread t_oMyThread(boost::bind(my_func, ref(x), ref(k)));
+	//boost::this_thread::sleep(boost::posix_time::seconds(2));
+	t_oMyThread.Join();
+	return 0;
+}
